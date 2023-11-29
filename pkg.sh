3f323060300bc07416b2483cef79f97719af0800
sebiik <sebiik@gmail.com>
2023-11-29 14:54:19 +0800

    create packages in packagetemp folder

--- pkg.sh
+++ pkg.sh
@@ -13,106 +13,115 @@ echo $VERSION_LONG
 
 
 # Init
-rm -rf package
-mkdir -p package
+rm -rf packagetemp
+mkdir -p packagetemp
 
 
 # Linux
 java -jar ./jdks/packr-all-4.0.0.jar --verbose ./jdks/packr-linux-x64.json
 
-rm -rf package/linux/ksoloti-${VERSION}/chibios/demos
-rm -rf package/linux/ksoloti-${VERSION}/chibios/test
-rm -rf package/linux/ksoloti-${VERSION}/chibios/testhal
+rm -rf packagetemp/linux/ksoloti-${VERSION}/chibios/demos
+rm -rf packagetemp/linux/ksoloti-${VERSION}/chibios/test
+rm -rf packagetemp/linux/ksoloti-${VERSION}/chibios/testhal
 
-rm -rf package/linux/ksoloti-${VERSION}/firmware/.dep
-rm -rf package/linux/ksoloti-${VERSION}/firmware/.settings
+rm -rf packagetemp/linux/ksoloti-${VERSION}/firmware/.dep
+rm -rf packagetemp/linux/ksoloti-${VERSION}/firmware/.settings
 
-rm -rf package/linux/ksoloti-${VERSION}/firmware/build/lst
-rm -rf package/linux/ksoloti-${VERSION}/firmware/build/obj
+rm -rf packagetemp/linux/ksoloti-${VERSION}/firmware/build/lst
+rm -rf packagetemp/linux/ksoloti-${VERSION}/firmware/build/obj
 
-rm -rf package/linux/ksoloti-${VERSION}/firmware/flasher/.dep
-rm -rf package/linux/ksoloti-${VERSION}/firmware/flasher/flasher_build/lst
-rm -rf package/linux/ksoloti-${VERSION}/firmware/flasher/flasher_build/obj
+rm -rf packagetemp/linux/ksoloti-${VERSION}/firmware/flasher/.dep
+rm -rf packagetemp/linux/ksoloti-${VERSION}/firmware/flasher/flasher_build/lst
+rm -rf packagetemp/linux/ksoloti-${VERSION}/firmware/flasher/flasher_build/obj
 
-rm -rf package/linux/ksoloti-${VERSION}/firmware/mounter/.dep
-rm -rf package/linux/ksoloti-${VERSION}/firmware/mounter/mounter_build/lst
-rm -rf package/linux/ksoloti-${VERSION}/firmware/mounter/mounter_build/obj
+rm -rf packagetemp/linux/ksoloti-${VERSION}/firmware/mounter/.dep
+rm -rf packagetemp/linux/ksoloti-${VERSION}/firmware/mounter/mounter_build/lst
+rm -rf packagetemp/linux/ksoloti-${VERSION}/firmware/mounter/mounter_build/obj
 
-rm -rf package/linux/ksoloti-${VERSION}/platform_linux/share
-rm -rf package/linux/ksoloti-${VERSION}/platform_linux/src
-rm -rf package/linux/ksoloti-${VERSION}/platform_linux/arm-none-eabi/lib/armv6-m
-rm -rf package/linux/ksoloti-${VERSION}/platform_linux/arm-none-eabi/lib/armv7-ar
-rm -rf package/linux/ksoloti-${VERSION}/platform_linux/arm-none-eabi/lib/armv7-m
-rm -rf package/linux/ksoloti-${VERSION}/platform_linux/lib/gcc/arm-none-eabi/4.9.3/armv6-m
-rm -rf package/linux/ksoloti-${VERSION}/platform_linux/lib/gcc/arm-none-eabi/4.9.3/armv7-ar
-rm -rf package/linux/ksoloti-${VERSION}/platform_linux/lib/gcc/arm-none-eabi/4.9.3/armv7-m
+rm -rf packagetemp/linux/ksoloti-${VERSION}/platform_linux/share
+rm -rf packagetemp/linux/ksoloti-${VERSION}/platform_linux/src
+rm -rf packagetemp/linux/ksoloti-${VERSION}/platform_linux/arm-none-eabi/lib/armv6-m
+rm -rf packagetemp/linux/ksoloti-${VERSION}/platform_linux/arm-none-eabi/lib/armv7-ar
+rm -rf packagetemp/linux/ksoloti-${VERSION}/platform_linux/arm-none-eabi/lib/armv7-m
+rm -rf packagetemp/linux/ksoloti-${VERSION}/platform_linux/lib/gcc/arm-none-eabi/4.9.3/armv6-m
+rm -rf packagetemp/linux/ksoloti-${VERSION}/platform_linux/lib/gcc/arm-none-eabi/4.9.3/armv7-ar
+rm -rf packagetemp/linux/ksoloti-${VERSION}/platform_linux/lib/gcc/arm-none-eabi/4.9.3/armv7-m
 
-cd package/linux && tar -czf ../ksoloti-linux-${VERSION_LONG}.tar.gz *
+rm -rf packagetemp/linux/ksoloti-${VERSION}/jre/jmods
+rm packagetemp/linux/ksoloti-${VERSION}/jre/lib/src.zip
+
+cd packagetemp/linux && tar -czf ../ksoloti-linux-${VERSION_LONG}.tar.gz *
 cd ../..
-rm -rf package/linux
+rm -rf packagetemp/linux
 
 
 # MacOS
 java -jar ./jdks/packr-all-4.0.0.jar --verbose ./jdks/packr-mac-x64.json
 
-rm -rf package/mac/Ksoloti.app/Contents/Resources/chibios/demos
-rm -rf package/mac/Ksoloti.app/Contents/Resources/chibios/test
-rm -rf package/mac/Ksoloti.app/Contents/Resources/chibios/testhal
+rm -rf packagetemp/mac/Ksoloti.app/Contents/Resources/chibios/demos
+rm -rf packagetemp/mac/Ksoloti.app/Contents/Resources/chibios/test
+rm -rf packagetemp/mac/Ksoloti.app/Contents/Resources/chibios/testhal
 
-rm -rf package/mac/Ksoloti.app/Contents/Resources/firmware/.dep
-rm -rf package/mac/Ksoloti.app/Contents/Resources/firmware/.settings
+rm -rf packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/.dep
+rm -rf packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/.settings
 
-rm -rf package/mac/Ksoloti.app/Contents/Resources/firmware/build/lst
-rm -rf package/mac/Ksoloti.app/Contents/Resources/firmware/build/obj
+rm -rf packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/build/lst
+rm -rf packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/build/obj
 
-rm -rf package/mac/Ksoloti.app/Contents/Resources/firmware/flasher/.dep
-rm -rf package/mac/Ksoloti.app/Contents/Resources/firmware/flasher/flasher_build/lst
-rm -rf package/mac/Ksoloti.app/Contents/Resources/firmware/flasher/flasher_build/obj
+rm -rf packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/flasher/.dep
+rm -rf packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/flasher/flasher_build/lst
+rm -rf packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/flasher/flasher_build/obj
 
-rm -rf package/mac/Ksoloti.app/Contents/Resources/firmware/mounter/.dep
-rm -rf package/mac/Ksoloti.app/Contents/Resources/firmware/mounter/mounter_build/lst
-rm -rf package/mac/Ksoloti.app/Contents/Resources/firmware/mounter/mounter_build/obj
+rm -rf packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/mounter/.dep
+rm -rf packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/mounter/mounter_build/lst
+rm -rf packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/mounter/mounter_build/obj
 
-chmod +x package/mac/Ksoloti.app/Contents/Resources/jre/lib/jspawnhelper
+rm -rf packagetemp/mac/Ksoloti.app/Contents/Resources/jre/jmods
+rm packagetemp/mac/Ksoloti.app/Contents/Resources/jre/lib/src.zip
 
-cd package/mac && zip -q -r ../ksoloti-mac-${VERSION_LONG}.zip *
+chmod +x packagetemp/mac/Ksoloti.app/Contents/Resources/jre/lib/jspawnhelper
+
+cd packagetemp/mac && zip -q -r ../ksoloti-mac-${VERSION_LONG}.zip *
 cd ../..
-rm -rf package/mac
+rm -rf packagetemp/mac
 
 
 # Windows
 java -jar ./jdks/packr-all-4.0.0.jar --verbose ./jdks/packr-win-x64.json
 
-rm -rf package/win/ksoloti-${VERSION}/chibios/demos
-rm -rf package/win/ksoloti-${VERSION}/chibios/test
-rm -rf package/win/ksoloti-${VERSION}/chibios/testhal
+rm -rf packagetemp/win/ksoloti-${VERSION}/chibios/demos
+rm -rf packagetemp/win/ksoloti-${VERSION}/chibios/test
+rm -rf packagetemp/win/ksoloti-${VERSION}/chibios/testhal
 
-rm -rf package/win/ksoloti-${VERSION}/firmware/.dep
-rm -rf package/win/ksoloti-${VERSION}/firmware/.settings
+rm -rf packagetemp/win/ksoloti-${VERSION}/firmware/.dep
+rm -rf packagetemp/win/ksoloti-${VERSION}/firmware/.settings
 
-rm -rf package/win/ksoloti-${VERSION}/firmware/build/lst
-rm -rf package/win/ksoloti-${VERSION}/firmware/build/obj
+rm -rf packagetemp/win/ksoloti-${VERSION}/firmware/build/lst
+rm -rf packagetemp/win/ksoloti-${VERSION}/firmware/build/obj
 
-rm -rf package/win/ksoloti-${VERSION}/firmware/flasher/.dep
-rm -rf package/win/ksoloti-${VERSION}/firmware/flasher/flasher_build/lst
-rm -rf package/win/ksoloti-${VERSION}/firmware/flasher/flasher_build/obj
+rm -rf packagetemp/win/ksoloti-${VERSION}/firmware/flasher/.dep
+rm -rf packagetemp/win/ksoloti-${VERSION}/firmware/flasher/flasher_build/lst
+rm -rf packagetemp/win/ksoloti-${VERSION}/firmware/flasher/flasher_build/obj
 
-rm -rf package/win/ksoloti-${VERSION}/firmware/mounter/.dep
-rm -rf package/win/ksoloti-${VERSION}/firmware/mounter/mounter_build/lst
-rm -rf package/win/ksoloti-${VERSION}/firmware/mounter/mounter_build/obj
+rm -rf packagetemp/win/ksoloti-${VERSION}/firmware/mounter/.dep
+rm -rf packagetemp/win/ksoloti-${VERSION}/firmware/mounter/mounter_build/lst
+rm -rf packagetemp/win/ksoloti-${VERSION}/firmware/mounter/mounter_build/obj
 
-rm -rf package/win/ksoloti-${VERSION}/platform_win/share
-rm -rf package/win/ksoloti-${VERSION}/platform_win/src
-rm -rf package/win/ksoloti-${VERSION}/platform_win/arm-none-eabi/lib/armv6-m
-rm -rf package/win/ksoloti-${VERSION}/platform_win/arm-none-eabi/lib/armv7-ar
-rm -rf package/win/ksoloti-${VERSION}/platform_win/arm-none-eabi/lib/armv7-m
-rm -rf package/win/ksoloti-${VERSION}/platform_win/lib/gcc/arm-none-eabi/4.9.3/armv6-m
-rm -rf package/win/ksoloti-${VERSION}/platform_win/lib/gcc/arm-none-eabi/4.9.3/armv7-ar
-rm -rf package/win/ksoloti-${VERSION}/platform_win/lib/gcc/arm-none-eabi/4.9.3/armv7-mo
+rm -rf packagetemp/win/ksoloti-${VERSION}/platform_win/share
+rm -rf packagetemp/win/ksoloti-${VERSION}/platform_win/src
+rm -rf packagetemp/win/ksoloti-${VERSION}/platform_win/arm-none-eabi/lib/armv6-m
+rm -rf packagetemp/win/ksoloti-${VERSION}/platform_win/arm-none-eabi/lib/armv7-ar
+rm -rf packagetemp/win/ksoloti-${VERSION}/platform_win/arm-none-eabi/lib/armv7-m
+rm -rf packagetemp/win/ksoloti-${VERSION}/platform_win/lib/gcc/arm-none-eabi/4.9.3/armv6-m
+rm -rf packagetemp/win/ksoloti-${VERSION}/platform_win/lib/gcc/arm-none-eabi/4.9.3/armv7-ar
+rm -rf packagetemp/win/ksoloti-${VERSION}/platform_win/lib/gcc/arm-none-eabi/4.9.3/armv7-mo
 
-cd package/win && zip -q -r ../ksoloti-windows-${VERSION_LONG}.zip *
+rm -rf packagetemp/win/ksoloti-${VERSION}/jre/jmods
+rm packagetemp/win/ksoloti-${VERSION}/jre/lib/src.zip
+
+cd packagetemp/win && zip -q -r ../ksoloti-windows-${VERSION_LONG}.zip *
 cd ../..
-rm -rf package/win
+rm -rf packagetemp/win
 
 
 # Cleanup
