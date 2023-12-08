31c284151ac8c7ab1f657f9679377c1642607282
sebiik <sebiik@gmail.com>
2023-12-09 00:21:11 +0800

    add two new component types

---
+++ src/main/java/components/EditorPaneComponent.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) 2013, 2014 Johannes Taelman
+ *
+ * This file is part of Axoloti.
+ *
+ * Axoloti is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Axoloti is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Axoloti. If not, see <http://www.gnu.org/licenses/>.
+ */
+package components;
+import axoloti.Theme;
+import axoloti.utils.Constants;
+import java.awt.Dimension;
+import javax.swing.JEditorPane;
+
+/**
+ *
+ * @author Johannes Taelman, sebiik
+ */
+public class EditorPaneComponent extends JEditorPane {
+
+    public EditorPaneComponent() {
+        setContentType("text/html");
+        setMaximumSize(new Dimension(5000,5000));
+        setMinimumSize(new Dimension(40,13));
+        setFont(Constants.FONT);
+        setBackground(Theme.getCurrentTheme().Object_Default_Background);
+        setForeground(Theme.getCurrentTheme().Object_Label_Text);
+        setEditable(false);
+        setFocusable(false);
+    }
+
+    public EditorPaneComponent(String text) {
+        setContentType("text/html");
+        setText(text);
+        setMaximumSize(new Dimension(5000,5000));
+        setMinimumSize(new Dimension(40,13));
+        setFont(Constants.FONT);
+        setBackground(Theme.getCurrentTheme().Object_Default_Background);
+        setForeground(Theme.getCurrentTheme().Object_Label_Text);
+        setEditable(false);
+        setFocusable(false);
+    }
+}
