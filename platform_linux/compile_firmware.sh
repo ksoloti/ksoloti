b2222008d853a109e5d5ad9be6863c5b6a5db886
sebiik <sebiik@gmail.com>
2024-01-28 00:13:09 +0800

    add echos to compile firmware script

--- platform_linux/compile_firmware.sh
+++ platform_linux/compile_firmware.sh
@@ -8,7 +8,9 @@ export axoloti_firmware=${axoloti_firmware:="$axoloti_release/firmware"}
 export axoloti_legacy_firmware=${axoloti_fegacy_firmware:="$axoloti_release/firmware_axoloti_legacy"}
 export axoloti_home=${axoloti_home:="$platformdir/.."}
 
+echo "\n------ ksoloti firmware ------"
 cd "${axoloti_firmware}"
 "${axoloti_firmware}/compile_firmware_linux.sh" $1
+echo "\n------ axo legacy firmware ------"
 cd ..
 "${axoloti_legacy_firmware}/compile_firmware_linux.sh" $1
