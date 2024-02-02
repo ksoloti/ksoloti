5d330498d05538881216146adf3a8b2fdb7ec5e9
sebiik <sebiik@gmail.com>
2024-02-02 15:10:21 +0800

    Merge branch 'master' into spilink-standalone

--- platform_linux/compile_firmware.sh
+++ platform_linux/compile_firmware.sh
@@ -5,7 +5,7 @@ platformdir="$(dirname $(readlink -f $0))"
 export axoloti_release=${axoloti_release:="$platformdir/.."}
 export axoloti_runtime=${axoloti_runtime:="$platformdir/.."}
 export axoloti_firmware=${axoloti_firmware:="$axoloti_release/firmware"}
-export axoloti_legacy_firmware=${axoloti_fegacy_firmware:="$axoloti_release/firmware_axoloti_legacy"}
+export axoloti_legacy_firmware=${axoloti_legacy_firmware:="$axoloti_release/firmware_axoloti_legacy"}
 export axoloti_home=${axoloti_home:="$platformdir/.."}
 
 echo "\n------ axo legacy firmware ------"
