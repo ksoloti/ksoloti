0b1e2073d1084e3585f0858fc95c81d952be4c2b
sebiik@gmail.com <sebiik@gmail.com>
2024-03-30 16:52:04 +0800

    code reformat

--- firmware/axoloti_memory.c
+++ firmware/axoloti_memory.c
@@ -1,6 +1,4 @@
 #include <ch.h>
-//#include <stddef.h>
-//#include <stdint.h>
 #include "axoloti_memory.h"
 #include "exceptions.h"
 #include "pconnection.h"
@@ -11,42 +9,51 @@ static void *sdram_last_addr;
 static void *sdram_end_addr;
 static int32_t sdram_total_remaining;
 
+
 void sdram_init(char *base_addr, char *end_addr) {
-  // assume base-address is a multiple of 4
-  sdram_base_addr = base_addr;
-  sdram_current_addr = base_addr;
-  sdram_end_addr = end_addr;
-  sdram_last_addr = 0;
-  sdram_total_remaining = end_addr - base_addr;
+    /* Assume base address is a multiple of 4 */
+    sdram_base_addr       = base_addr;
+    sdram_current_addr    = base_addr;
+    sdram_end_addr        = end_addr;
+    sdram_last_addr       = 0;
+    sdram_total_remaining = end_addr - base_addr;
 }
 
-void* sdram_malloc(size_t size) {
-  // round up size to a multiple of 4
-  if (size & 3)
-    size = (size & ~3) + 4;
 
-  if (sdram_current_addr == 0) return 0; // not initialized
+void *sdram_malloc(size_t size) {
+    /* Round up size to a multiple of 4 */
+    if (size & 3) {
+        size = (size & ~3) + 4;
+    }
 
-  sdram_total_remaining -= size;
+    if (sdram_current_addr == 0) {
+        /* Not initialized */
+        return 0; 
+    }
 
-  if (sdram_current_addr + size > sdram_end_addr) {
-    report_patchLoadSDRamOverflow("m",sdram_total_remaining);
-    chThdSleepMilliseconds(200); // get the report out
-    return sdram_base_addr; // not available, but return valid area to avoid a memory fault
-  }
+    sdram_total_remaining -= size;
 
-  sdram_last_addr = sdram_current_addr;
-  sdram_current_addr += size;
-  return sdram_last_addr;
+    if (sdram_current_addr + size > sdram_end_addr) {
+        report_patchLoadSDRamOverflow("m", sdram_total_remaining);
+        chThdSleepMilliseconds(200); /* Get the report out */
+        return sdram_base_addr;      /* Not available, but return valid area to avoid a memory fault */
+    }
+
+    sdram_last_addr = sdram_current_addr;
+    sdram_current_addr += size;
+
+    return sdram_last_addr;
 }
+
 
 void sdram_free(void *ptr) {
-  if (ptr == sdram_last_addr) {
-    sdram_total_remaining += sdram_current_addr - sdram_last_addr;
-    sdram_current_addr = sdram_last_addr;
-  }
+    if (ptr == sdram_last_addr) {
+        sdram_total_remaining += sdram_current_addr - sdram_last_addr;
+        sdram_current_addr = sdram_last_addr;
+    }
 }
 
+
 int32_t sdram_get_free(void) {
-  return sdram_total_remaining;
+    return sdram_total_remaining;
 }
