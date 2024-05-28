4ddb5bebcacf90ca8d7da7e1fd5ef7951b7c818f
sebiik@gmail.com <sebiik@gmail.com>
2024-05-28 11:26:45 +0800

    update gitignores

--- platform_linux/compile_firmware.sh
+++ platform_linux/compile_firmware.sh
@@ -8,9 +8,9 @@ export axoloti_firmware=${axoloti_firmware:="$axoloti_release/firmware"}
 export axoloti_legacy_firmware=${axoloti_legacy_firmware:="$axoloti_release/firmware_axoloti_legacy"}
 export axoloti_home=${axoloti_home:="$platformdir/.."}
 
-echo "\n\n\n-------------------------- compiling axo legacy firmware --------------------------"
+echo "\n\n\n-----------------------------------------------------------------------------------"
 "${axoloti_legacy_firmware}/compile_firmware_linux.sh" $1
-echo "\n\n\n-------------------------- compiling ksoloti firmware --------------------------"
+echo "\n\n\n-----------------------------------------------------------------------------------"
 cd ..
 cd "${axoloti_firmware}"
 "${axoloti_firmware}/compile_firmware_linux.sh" $1
