dc64063e0be41d6656513d5f295389f5c776d96e
sebiik <sebiik@gmail.com>
2024-06-05 16:49:11 +0800

    decrease -Xms, increase -Xmx in java app

--- win_byld_java.sh
+++ win_byld_java.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ant && java -Xms128m -Xmx1g -Xbootclasspath/a:lib/marlin-0.9.4.8-Unsafe-OpenJDK11.jar -Dsun.java2d.renderer=org.marlin.pisces.MarlinRenderingEngine -jar ./dist/Ksoloti.jar
+ant && java -Xms256m -Xmx2g -Xbootclasspath/a:lib/marlin-0.9.4.8-Unsafe-OpenJDK11.jar -Dsun.java2d.renderer=org.marlin.pisces.MarlinRenderingEngine -jar ./dist/Ksoloti.jar
