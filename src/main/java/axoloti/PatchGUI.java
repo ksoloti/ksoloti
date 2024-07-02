a3aee65a4c24a50d9f34287664524c44c935ead1
sebiik <sebiik@gmail.com>
2024-07-02 16:03:43 +0800

    improve patch title handling for subpatches

--- src/main/java/axoloti/PatchGUI.java
+++ src/main/java/axoloti/PatchGUI.java
@@ -902,7 +902,7 @@ public class PatchGUI extends Patch {
             String str = FileNamePath.substring(brk) + "   [" + FileNamePath.substring(0, brk) + "]";
             patchframe.setTitle(str);
         }
-        else if (!FileNamePath.equals("untitled")) {
+        else if (patchframe.getPatch().getSettings() != null && patchframe.getPatch().getSettings().subpatchmode != SubPatchMode.no) {
             /* Subpatch */
             patchframe.setTitle(FileNamePath + "   (subpatch)");
             patchframe.setSaveMenuEnabled(false); /* parent has to be saved to preserve changes */
