29f022fa2ac4140713c096157a9732b2c19b2b58
sebiik <sebiik@gmail.com>
2024-07-17 10:29:40 +0800

    kompile_shortcut usable on mac, windows in bash

--- kompile_shortcut.sh
+++ kompile_shortcut.sh
@@ -1,3 +1,43 @@
 #!/bin/bash
-# sh ./qlean.sh
-sh ./platform_linux/compile_firmware.sh 2>&1 | tee firmware.log
+
+platform='unknown'
+unamestr=`uname`
+case "$unamestr" in
+	Linux)
+		platform='linux'
+		rootdir="$(dirname $(readlink -f $0))"
+	;;
+	Darwin)
+		platform='mac'
+		rootdir="$(cd $(dirname $0); pwd -P)"
+	;;
+	MINGW*)
+		platform='windows'
+		rootdir="$(cd $(dirname $0); pwd -P)"
+	;;
+        *)
+                echo "Unknown OS: $unamestr - aborting..."
+                exit
+        ;;
+esac
+
+case "$platform" in
+        mac)
+            # sh ./qlean.sh
+            sh ./platform_osx/compile_firmware.sh BOARD_AXOLOTI_CORE && ./platform_osx/compile_firmware.sh BOARD_KSOLOTI_CORE 2>&1 | tee firmware.log
+        ;;
+        linux)
+            # sh ./qlean.sh
+            sh ./platform_linux/compile_firmware.sh 2>&1 | tee firmware.log
+            sh ./platform_linux/compile_firmware.sh BOARD_AXOLOTI_CORE && ./platform_linux/compile_firmware.sh BOARD_KSOLOTI_CORE 2>&1 | tee firmware.log
+        ;;
+        windows)
+            # sh ./qlean.sh
+            # echo "This script currently does not support Windows."
+            cd platform_win
+            cmd "//C path.bat && compile_firmware.bat BOARD_AXOLOTI_CORE && compile_firmware.bat BOARD_KSOLOTI_CORE"
+            cd ..
+            
+        ;;
+esac
+
