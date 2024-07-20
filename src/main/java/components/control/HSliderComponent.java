b4c05ef483e3950bf384d360034e0ad940658e94
sebiik <sebiik@gmail.com>
2024-07-20 23:02:22 +0800

    rework modulation sliders (Hslider) now has most functions any other paramters have

--- src/main/java/components/control/HSliderComponent.java
+++ src/main/java/components/control/HSliderComponent.java
@@ -18,13 +18,20 @@
  */
 package components.control;
 
+import axoloti.MainFrame;
 import axoloti.utils.KeyUtils;
+import axoloti.utils.Preferences;
 
 import java.awt.BasicStroke;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.Robot;
+import java.awt.Stroke;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Rectangle2D;
@@ -38,14 +45,33 @@ public class HSliderComponent extends ACtrlComponent {
     double value = 0;
     double max = 128;
     double min = -128;
-    int px;
+    private int MousePressedCoordX;
+    private int MousePressedCoordY;
+    private String keybBuffer = "";
+
+    private Robot robot;
+
 
     public HSliderComponent() {
-        Dimension d = new Dimension(160, 12);
+        Dimension d = new Dimension(256, 12);
         setMinimumSize(d);
         setPreferredSize(d);
         setMaximumSize(d);
         setSize(d);
+        setToolTipText("Double-click to reset to 0.\n" +
+                       "Type a number and hit Enter to set\n" +
+                       "the value. Hit Escape to cancel.");
+        addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                keybBuffer = "";
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                keybBuffer = "";
+            }
+        });
         SetupTransferHandler();
     }
 
@@ -58,23 +84,32 @@ public class HSliderComponent extends ACtrlComponent {
         if (e.isShiftDown()) {
             t = t * 0.1;
         }
-        setValue(value + t * (double)(e.getX() - px));
-        px = e.getX();
+        double v = value - t * ((int) Math.round((MousePressedCoordX - e.getXOnScreen())));
+        robotMoveToCenter();
+        if (robot == null) {
+            MousePressedCoordX = e.getXOnScreen();
+        }
+        setValue(v);
     }
 
     @Override
     protected void mousePressed(MouseEvent e) {
-        px = e.getX();
-        e.consume();
-        fireEventAdjustmentBegin();
-    }
-
-    @Override
-    public void keyPressed(KeyEvent ke) {
-    }
-
-    @Override
-    void keyReleased(KeyEvent ke) {
+        if (!e.isPopupTrigger()) {
+            if ((e.getClickCount() == 2) && (e.getButton() == MouseEvent.BUTTON1)) {
+                setValue(0);
+            }
+            else {
+                grabFocus();
+                MousePressedCoordX = e.getXOnScreen();
+                MousePressedCoordY = e.getYOnScreen();
+                robot = createRobot();
+                if (!Preferences.LoadPreferences().getMouseDoNotRecenterWhenAdjustingControls()) {
+                    getRootPane().setCursor(MainFrame.transparentCursor);
+                }
+            }
+            e.consume();
+            fireEventAdjustmentBegin();
+        }
     }
 
     @Override
@@ -83,7 +118,120 @@ public class HSliderComponent extends ACtrlComponent {
             fireEventAdjustmentFinished();
             e.consume();
         }
+        getRootPane().setCursor(Cursor.getDefaultCursor());
+        robot = null;
     }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+        double steps = 1.0;
+        if (ke.isShiftDown()) {
+            steps = steps * 0.1; // mini steps!
+            if (KeyUtils.isControlOrCommandDown(ke)) {
+                steps = steps * 0.1; // micro steps!                
+            }
+        } else if (KeyUtils.isControlOrCommandDown(ke)) {
+            steps = steps * 10.0; //accelerate!
+        }
+        switch (ke.getKeyCode()) {
+            case KeyEvent.VK_UP:
+            case KeyEvent.VK_RIGHT:
+                fireEventAdjustmentBegin();
+                setValue(getValue() + steps);
+                ke.consume();
+                break;
+            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_LEFT:
+                fireEventAdjustmentBegin();
+                setValue(getValue() - steps);
+                ke.consume();
+                break;
+            case KeyEvent.VK_PAGE_UP:
+                fireEventAdjustmentBegin();
+                setValue(getValue() + 5 * steps);
+                ke.consume();
+                break;
+            case KeyEvent.VK_PAGE_DOWN:
+                fireEventAdjustmentBegin();
+                setValue(getValue() - 5 * steps);
+                ke.consume();
+                break;
+            case KeyEvent.VK_HOME:
+                fireEventAdjustmentBegin();
+                setValue(max);
+                fireEventAdjustmentFinished();
+                ke.consume();
+                break;
+            case KeyEvent.VK_END:
+                fireEventAdjustmentBegin();
+                setValue(min);
+                fireEventAdjustmentFinished();
+                ke.consume();
+                break;
+            case KeyEvent.VK_ENTER:
+                fireEventAdjustmentBegin();
+                try {
+                    setValue(Float.parseFloat(keybBuffer));
+                } catch (java.lang.NumberFormatException ex) {
+                }
+                fireEventAdjustmentFinished();
+                keybBuffer = "";
+                ke.consume();
+                repaint();
+                break;
+            case KeyEvent.VK_BACK_SPACE:
+                if (keybBuffer.length() > 0) {
+                    keybBuffer = keybBuffer.substring(0, keybBuffer.length() - 1);
+                }
+                ke.consume();
+                repaint();
+                break;
+            case KeyEvent.VK_ESCAPE:
+                keybBuffer = "";
+                ke.consume();
+                repaint();
+                break;
+            default:
+        }
+        switch (ke.getKeyChar()) {
+            case '-':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            case '0':
+            case '.':
+                keybBuffer += ke.getKeyChar();
+                ke.consume();
+                repaint();
+                break;
+            default:
+        }
+    }
+
+    @Override
+    void keyReleased(KeyEvent ke) {
+        switch (ke.getKeyCode()) {
+            case KeyEvent.VK_UP:
+            case KeyEvent.VK_RIGHT:
+            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_LEFT:
+            case KeyEvent.VK_PAGE_UP:
+            case KeyEvent.VK_PAGE_DOWN:
+                fireEventAdjustmentFinished();
+                ke.consume();
+                break;
+            default:
+        }
+    }
+
+    private static final Stroke strokeThin = new BasicStroke(1);
+    private static final Stroke strokeThick = new BasicStroke(2);
 
     @Override
     public void paintComponent(Graphics g) {
@@ -93,15 +241,28 @@ public class HSliderComponent extends ACtrlComponent {
                 RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
                 RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+
         int margin = 50;
         int bwidth = getWidth() - margin;
+
+        g2.setColor(getBackground());
+        g2.fillRect(0, 0, bwidth, getHeight() - 1);
         g2.setPaint(getForeground());
+        if (isFocusOwner()) {
+            g2.setStroke(strokeThick);
+        } else {
+            g2.setStroke(strokeThin);
+        }
         g2.drawRect(0, 0, bwidth, getHeight() - 1);
+
         int p = (int) (1 + ((value - min) * (bwidth - 2)) / (max - min));
         int p1 = (int) (1 + ((0 - min) * (bwidth - 2)) / (max - min));
+
+        g2.setStroke(strokeThin);
         g2.drawLine(p, getHeight() / 2, p1, getHeight() / 2);
         g2.setStroke(new BasicStroke(2));
         g2.drawLine(p, 0, p, getHeight());
+
         String s = String.format("%5.2f", value);
         Rectangle2D r = g2.getFontMetrics().getStringBounds(s, g);
         g2.drawString(s, bwidth + (margin / 2) - (int) (0.5 + r.getWidth() / 2), getHeight());
@@ -139,5 +300,13 @@ public class HSliderComponent extends ACtrlComponent {
 
     public double getMaximum() {
         return max;
+    }
+
+    @Override
+    public void robotMoveToCenter() {
+        if (robot != null) {
+            getRootPane().setCursor(MainFrame.transparentCursor);
+            robot.mouseMove(MousePressedCoordX, MousePressedCoordY);
+        }
     }
 }
