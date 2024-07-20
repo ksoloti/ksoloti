7d9c6a85a55d9cf77d35ee67afd743a27a2ce399
sebiik <sebiik@gmail.com>
2024-07-20 22:59:44 +0800

    fix win_byld_java script

--- win_byld_java.sh
+++ win_byld_java.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-ant clean && ant && java -Xms256m -Xmx2g -Xbootclasspath/a:lib/marlin-0.9.4.8-Unsafe-OpenJDK11.jar -Dsun.java2d.renderer=org.marlin.pisces.MarlinRenderingEngine -jar ./dist/Ksoloti.jar
+./platform_win/apache-ant-1.10.14/bin/ant.bat clean && \
+./platform_win/apache-ant-1.10.14/bin/ant.bat && \
+java -Xms256m -Xmx2g -Xbootclasspath/a:lib/marlin-0.9.4.8-Unsafe-OpenJDK11.jar -Dsun.java2d.renderer=org.marlin.pisces.MarlinRenderingEngine -jar ./dist/Ksoloti.jar
