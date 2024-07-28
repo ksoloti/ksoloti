28c063b4c4a4b0028b66a8ecdaf3ce41fffb3cd2
sebiik <sebiik@gmail.com>
2024-07-28 16:40:58 +0800

    fix buffer exception on windows while running automated test compilation

--- src/main/java/axoloti/MainFrame.java
+++ src/main/java/axoloti/MainFrame.java
@@ -959,6 +959,7 @@ public final class MainFrame extends javax.swing.JFrame implements ActionListene
             boolean status;
             PatchGUI patch1 = serializer.read(PatchGUI.class, f);
             PatchFrame pf = new PatchFrame(patch1, qcmdprocessor);
+            pf.createBufferStrategy(1);
             patch1.setFileNamePath(f.getPath());
             patch1.PostContructor();
             patch1.WriteCode();
