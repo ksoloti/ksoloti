882f2e16fe4ee5644aea8c70a7e97a8504a93728
sebiik <sebiik@gmail.com>
2024-08-05 09:46:35 +0800

    don't start patcher automatically after building java app

--- win_byld_java.sh
+++ win_byld_java.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./platform_win/apache-ant-1.10.14/bin/ant.bat clean && \
-./platform_win/apache-ant-1.10.14/bin/ant.bat && \
-java -Xms256m -Xmx2g -Xbootclasspath/a:lib/marlin-0.9.4.8-Unsafe-OpenJDK11.jar -Dsun.java2d.renderer=org.marlin.pisces.MarlinRenderingEngine -jar ./dist/Ksoloti.jar
+./platform_win/apache-ant-1.10.14/bin/ant.bat
+#java -Xms256m -Xmx2g -Xbootclasspath/a:lib/marlin-0.9.4.8-Unsafe-OpenJDK11.jar -Dsun.java2d.renderer=org.marlin.pisces.MarlinRenderingEngine -jar ./dist/Ksoloti.jar
