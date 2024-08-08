638e1008004b156b6523ae022761f210ef484752
sebiik <sebiik@gmail.com>
2024-08-08 18:21:08 +0800

    create lst files for flasher and mounter too

--- kompile_shortcut.sh
+++ kompile_shortcut.sh
@@ -25,15 +25,21 @@ esac
 case "$platform" in
         mac)
             sh ./platform_osx/compile_firmware.sh BOARD_AXOLOTI_CORE && ./platform_osx/compile_firmware.sh BOARD_KSOLOTI_CORE 2>&1 | tee firmware.log
+            sh arm-none-eabi-objdump -belf32-littlearm -marm --demangle --disassemble ../firmware/flasher/flasher_build/axoloti_flasher.elf > ../firmware/flasher/flasher_build/axoloti_flasher.lst && arm-none-eabi-objdump -belf32-littlearm -marm --demangle --disassemble ../firmware/flasher/flasher_build/ksoloti_flasher.elf > ../firmware/flasher/flasher_build/ksoloti_flasher.lst
+            sh arm-none-eabi-objdump -belf32-littlearm -marm --demangle --disassemble ../firmware/mounter/mounter_build/axoloti_mounter.elf > ../firmware/mounter/mounter_build/axoloti_mounter.lst && arm-none-eabi-objdump -belf32-littlearm -marm --demangle --disassemble ../firmware/mounter/mounter_build/ksoloti_mounter.elf > ../firmware/mounter/mounter_build/ksoloti_mounter.lst
             sh arm-none-eabi-objdump -belf32-littlearm -marm --demangle --disassemble ../firmware/build/axoloti.elf > ../firmware/build/axoloti.lst && arm-none-eabi-objdump -belf32-littlearm -marm --demangle --disassemble ../firmware/build/ksoloti.elf > ../firmware/build/ksoloti.lst
         ;;
         linux)
             sh ./platform_linux/compile_firmware.sh BOARD_AXOLOTI_CORE && ./platform_linux/compile_firmware.sh BOARD_KSOLOTI_CORE 2>&1 | tee firmware.log
+            sh arm-none-eabi-objdump -belf32-littlearm -marm --demangle --disassemble ../firmware/mounter/mounter_build/axoloti_mounter.elf > ../firmware/mounter/mounter_build/axoloti_mounter.lst && arm-none-eabi-objdump -belf32-littlearm -marm --demangle --disassemble ../firmware/mounter/mounter_build/ksoloti_mounter.elf > ../firmware/mounter/mounter_build/ksoloti_mounter.lst
+            sh arm-none-eabi-objdump -belf32-littlearm -marm --demangle --disassemble ../firmware/build/axoloti.elf > ../firmware/build/axoloti.lst && arm-none-eabi-objdump -belf32-littlearm -marm --demangle --disassemble ../firmware/build/ksoloti.elf > ../firmware/build/ksoloti.lst
             sh arm-none-eabi-objdump -belf32-littlearm -marm --demangle --disassemble ../firmware/build/axoloti.elf > ../firmware/build/axoloti.lst && arm-none-eabi-objdump -belf32-littlearm -marm --demangle --disassemble ../firmware/build/ksoloti.elf > ../firmware/build/ksoloti.lst
         ;;
         windows)
             cd platform_win
             cmd "//C path.bat && compile_firmware.bat BOARD_AXOLOTI_CORE 2>&1 | tee ..\firmware.log && compile_firmware.bat BOARD_KSOLOTI_CORE 2>&1 | tee -a ..\firmware.log"
+            cmd "//C path.bat && arm-none-eabi-objdump -belf32-littlearm -marm --demangle --disassemble ..\firmware\flasher\flasher_build\axoloti_flasher.elf > ..\firmware\flasher\flasher_build\axoloti_flasher.lst && arm-none-eabi-objdump -belf32-littlearm -marm --demangle --disassemble ..\firmware\flasher\flasher_build\ksoloti_flasher.elf > ..\firmware\flasher\flasher_build\ksoloti_flasher.lst"
+            cmd "//C path.bat && arm-none-eabi-objdump -belf32-littlearm -marm --demangle --disassemble ..\firmware\mounter\mounter_build\axoloti_mounter.elf > ..\firmware\mounter\mounter_build\axoloti_mounter.lst && arm-none-eabi-objdump -belf32-littlearm -marm --demangle --disassemble ..\firmware\mounter\mounter_build\ksoloti_mounter.elf > ..\firmware\mounter\mounter_build\ksoloti_mounter.lst"
             cmd "//C path.bat && arm-none-eabi-objdump -belf32-littlearm -marm --demangle --disassemble ..\firmware\build\axoloti.elf > ..\firmware\build\axoloti.lst && arm-none-eabi-objdump -belf32-littlearm -marm --demangle --disassemble ..\firmware\build\ksoloti.elf > ..\firmware\build\ksoloti.lst"
             cd ..
         ;;
