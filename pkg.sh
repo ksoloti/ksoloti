fff93ff775b9b764489f063fd37fbf71981544b6
sebiik <sebiik@gmail.com>
2024-08-30 15:04:12 +0800

    apply git tag version to packaged app folder / mac .app name

--- pkg.sh
+++ pkg.sh
@@ -25,18 +25,18 @@ esac
 START=$(date +%s)
 
 VERSION="$(git describe --tags | grep -Po '\d*\.\d*\.\d*' 2>&1)"
-echo $VERSION
-VERSION_LONG=
+VERSION_LONG="$(git describe --long --always --tags 2>&1)"
+CUSTOMLABEL=
 
-CUSTOMNAME=
 if [ $# -eq 1 ]
   then
-    CUSTOMNAME=$1
-    echo $CUSTOMNAME
-  else
-    VERSION_LONG="$(git describe --long --always --tags 2>&1)"
-    echo $VERSION_LONG
+    VERSION=$1
+    DASH="-"
+    CUSTOMLABEL=$1${DASH}
 fi
+
+echo $VERSION
+echo $VERSION_LONG
 
 # ----- Windows: Check if 7-Zip is installed
 case "$platform" in
@@ -90,7 +90,7 @@ rm -rf packagetemp/*
 
 
 # ----- Linux
-java -jar ./jdks/packr-all-4.0.0.jar --verbose ./jdks/packr-linux-x64.json
+java -jar ./jdks/packr-all-4.0.0.jar --verbose --output ./packagetemp/linux/ksoloti-${VERSION} -- ./jdks/packr-linux-x64.json
 
 rm -rf ./packagetemp/linux/ksoloti-${VERSION}/chibios/demos
 rm -rf ./packagetemp/linux/ksoloti-${VERSION}/chibios/test
@@ -135,66 +135,66 @@ rm -rf ./packagetemp/linux/ksoloti-${VERSION}/jre/demo
 rm -rf ./packagetemp/linux/ksoloti-${VERSION}/jre/man
 rm     ./packagetemp/linux/ksoloti-${VERSION}/jre/lib/src.zip
 
-cd ./packagetemp/linux && tar -czf ../ksoloti_patcher-linux-${VERSION_LONG}${CUSTOMNAME}.tar.gz *
+cd ./packagetemp/linux && tar -czf ../ksoloti_patcher-linux-${CUSTOMLABEL}${VERSION_LONG}.tar.gz *
 cd ../..
 rm -rf ./packagetemp/linux
 
 
 # ----- MacOS x64
-java -jar ./jdks/packr-all-4.0.0.jar --verbose ./jdks/packr-mac-x64.json
+java -jar ./jdks/packr-all-4.0.0.jar --verbose --output ./packagetemp/mac/Ksoloti-${VERSION}.app -- ./jdks/packr-mac-x64.json
 
-rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/chibios/demos
-rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/chibios/test
-rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/chibios/testhal
+rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/chibios/demos
+rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/chibios/test
+rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/chibios/testhal
 
-rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/.dep
-rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/.settings
+rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/firmware/.dep
+rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/firmware/.settings
 
-rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/build/lst
-rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/build/obj
-rm     ./packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/build/*.dmp
-rm     ./packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/build/*.lst
-rm     ./packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/build/*.map
+rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/firmware/build/lst
+rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/firmware/build/obj
+rm     ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/firmware/build/*.dmp
+rm     ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/firmware/build/*.lst
+rm     ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/firmware/build/*.map
 
-rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/flasher/.dep
-rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/flasher/flasher_build/lst
-rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/flasher/flasher_build/obj
-rm     ./packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/flasher/flasher_build/*.dmp
-rm     ./packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/flasher/flasher_build/*.lst
-rm     ./packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/flasher/flasher_build/*.map
+rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/firmware/flasher/.dep
+rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/firmware/flasher/flasher_build/lst
+rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/firmware/flasher/flasher_build/obj
+rm     ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/firmware/flasher/flasher_build/*.dmp
+rm     ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/firmware/flasher/flasher_build/*.lst
+rm     ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/firmware/flasher/flasher_build/*.map
 
-rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/mounter/.dep
-rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/mounter/mounter_build/lst
-rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/mounter/mounter_build/obj
-rm     ./packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/mounter/mounter_build/*.dmp
-rm     ./packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/mounter/mounter_build/*.lst
-rm     ./packagetemp/mac/Ksoloti.app/Contents/Resources/firmware/mounter/mounter_build/*.map
+rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/firmware/mounter/.dep
+rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/firmware/mounter/mounter_build/lst
+rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/firmware/mounter/mounter_build/obj
+rm     ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/firmware/mounter/mounter_build/*.dmp
+rm     ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/firmware/mounter/mounter_build/*.lst
+rm     ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/firmware/mounter/mounter_build/*.map
 
-rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/platform_osx/share
-rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/platform_osx/src
-# rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/platform_osx/arm-none-eabi/lib/armv6-m
-# rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/platform_osx/arm-none-eabi/lib/armv7-ar
-# rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/platform_osx/arm-none-eabi/lib/armv7-m
-# rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/platform_osx/arm-none-eabi/lib/cortex-m7
-# rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/platform_osx/lib/gcc/arm-none-eabi/4.9.3/armv6-m
-# rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/platform_osx/lib/gcc/arm-none-eabi/4.9.3/armv7-ar
-# rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/platform_osx/lib/gcc/arm-none-eabi/4.9.3/armv7-mo
-# rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/platform_osx/lib/gcc/arm-none-eabi/4.9.3/cortex-m7
+rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/platform_osx/share
+rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/platform_osx/src
+# rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/platform_osx/arm-none-eabi/lib/armv6-m
+# rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/platform_osx/arm-none-eabi/lib/armv7-ar
+# rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/platform_osx/arm-none-eabi/lib/armv7-m
+# rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/platform_osx/arm-none-eabi/lib/cortex-m7
+# rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/platform_osx/lib/gcc/arm-none-eabi/4.9.3/armv6-m
+# rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/platform_osx/lib/gcc/arm-none-eabi/4.9.3/armv7-ar
+# rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/platform_osx/lib/gcc/arm-none-eabi/4.9.3/armv7-mo
+# rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/platform_osx/lib/gcc/arm-none-eabi/4.9.3/cortex-m7
 
-rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/jre/jmods
-rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/jre/demo
-rm -rf ./packagetemp/mac/Ksoloti.app/Contents/Resources/jre/man
-rm     ./packagetemp/mac/Ksoloti.app/Contents/Resources/jre/lib/src.zip
+rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/jre/jmods
+rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/jre/demo
+rm -rf ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/jre/man
+rm     ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/jre/lib/src.zip
 
-chmod +x ./packagetemp/mac/Ksoloti.app/Contents/Resources/jre/lib/jspawnhelper
+chmod +x ./packagetemp/mac/Ksoloti-${VERSION}.app/Contents/Resources/jre/lib/jspawnhelper
 
-cd ./packagetemp/mac && tar -czf ../ksoloti_patcher-mac-${VERSION_LONG}${CUSTOMNAME}.tar.gz *
+cd ./packagetemp/mac && tar -czf ../ksoloti_patcher-mac-${CUSTOMLABEL}${VERSION_LONG}.tar.gz *
 cd ../..
 rm -rf ./packagetemp/mac
 
 
 # ----- Windows
-java -jar ./jdks/packr-all-4.0.0.jar --verbose ./jdks/packr-win-x64.json
+java -jar ./jdks/packr-all-4.0.0.jar --verbose --output ./packagetemp/win/ksoloti-${VERSION} -- ./jdks/packr-win-x64.json
 
 rm -rf ./packagetemp/win/ksoloti-${VERSION}/chibios/demos
 rm -rf ./packagetemp/win/ksoloti-${VERSION}/chibios/test
@@ -243,16 +243,16 @@ rm     ./packagetemp/win/ksoloti-${VERSION}/jre/lib/src.zip
 
 case "$platform" in
         mac)
-            cd ./packagetemp/win && zip -q -r ../ksoloti_patcher-windows-${VERSION_LONG}${CUSTOMNAME}.zip *
+            cd ./packagetemp/win && zip -q -r ../ksoloti_patcher-windows-${CUSTOMLABEL}${VERSION_LONG}.zip *
         ;;
         linux)
-            cd ./packagetemp/win && zip -q -r ../ksoloti_patcher-windows-${VERSION_LONG}${CUSTOMNAME}.zip *
+            cd ./packagetemp/win && zip -q -r ../ksoloti_patcher-windows-${CUSTOMLABEL}${VERSION_LONG}.zip *
         ;;
         windows)
             # apply icon
             ./jdks/rcedit-x64.exe ./packagetemp/win/ksoloti-${VERSION}/Ksoloti.exe --set-icon ./src/main/java/resources/ksoloti_icon.ico
             # zip using 7-zip
-            cd ./packagetemp/win && "C:/Program Files/7-Zip/7z.exe" a -tzip ../ksoloti_patcher-windows-${VERSION_LONG}${CUSTOMNAME}.zip *
+            cd ./packagetemp/win && "C:/Program Files/7-Zip/7z.exe" a -tzip ../ksoloti_patcher-windows-${CUSTOMLABEL}${VERSION_LONG}.zip *
         ;;
 esac
 cd ../..
@@ -260,4 +260,4 @@ rm -rf ./packagetemp/win
 
 
 END=$(date +%s)
-echo "All done! Elapsed time: $(($END-$START)) seconds."
+echo "All done! Elapsed time: $(((END - START) / 60)) min $(((END - START) % 60)) sec."
