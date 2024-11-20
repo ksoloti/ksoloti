c063f54039ee873bef1dc0e798c7499df95be6d9
sebiik <sebiik@gmail.com>
2024-11-20 15:49:40 +0800

    MI: add include to avoid M_PI (`?) error

--- firmware/mutable_instruments/stmlib/dsp/cosine_oscillator.h
+++ firmware/mutable_instruments/stmlib/dsp/cosine_oscillator.h
@@ -8,10 +8,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,7 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-// 
+//
 // See http://creativecommons.org/licenses/MIT/ for more information.
 //
 // -----------------------------------------------------------------------------
@@ -31,6 +31,7 @@
 #define STMLIB_DSP_COSINE_OSCILLATOR_H_
 
 #include "stmlib/stmlib.h"
+#include "stmlib/dsp/filter.h"
 
 #include <cmath>
 
@@ -60,7 +61,7 @@ class CosineOscillator {
     }
     Start();
   }
-  
+
   inline void InitApproximate(float frequency) {
     float sign = 16.0f;
     frequency -= 0.25f;
@@ -76,12 +77,12 @@ class CosineOscillator {
     iir_coefficient_ = sign * frequency * (1.0f - 2.0f * frequency);
     initial_amplitude_ = iir_coefficient_ * 0.25f;
   }
-  
+
   inline void Start() {
     y1_ = initial_amplitude_;
     y0_ = 0.5f;
   }
-  
+
   inline float value() const {
     return y1_ + 0.5f;
   }
@@ -92,7 +93,7 @@ class CosineOscillator {
     y1_ = temp;
     return temp + 0.5f;
   }
-  
+
  private:
   float y1_;
   float y0_;
