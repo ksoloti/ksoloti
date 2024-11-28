aae681e6293294bc6c157b55d65f82f640772779
sebiik <sebiik@gmail.com>
2024-11-28 18:47:52 +0800

    clean up qlean.sh ??? i really dont know anymore

--- qlean.sh
+++ qlean.sh
@@ -4,9 +4,13 @@ rm -f ./firmware/build/lst/*.*
 rm -f ./firmware/build/obj/*.*
 
 rm -f ./firmware/*/.dep/*.*
-rm -f ./firmware/*/build/lst/*.*
-rm -f ./firmware/*/build/obj/*.*
+rm -f ./firmware/*/lst/*.*
+rm -f ./firmware/*/obj/*.*
 
 rm -f ./firmware/*/*/.dep/*.*
-rm -f ./firmware/*/*/build/lst/*.*
-rm -f ./firmware/*/*/build/obj/*.*
+rm -f ./firmware/*/*/lst/*.*
+rm -f ./firmware/*/*/obj/*.*
+
+rm -f ./firmware/*/*/*/.dep/*.*
+rm -f ./firmware/*/*/*/lst/*.*
+rm -f ./firmware/*/*/*/obj/*.*
