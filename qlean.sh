7e6e0de33fc4b6b7313ff4d4b49626398a8d5049
sebiik <sebiik@gmail.com>
2024-11-28 18:39:03 +0800

    clean up qlean.sh =?? or so i like to believe

--- qlean.sh
+++ qlean.sh
@@ -2,9 +2,11 @@
 rm -f ./firmware/.dep/*.*
 rm -f ./firmware/build/lst/*.*
 rm -f ./firmware/build/obj/*.*
-rm -f ./firmware/flasher/.dep/*.*
-rm -f ./firmware/flasher/flasher_build/lst/*.*
-rm -f ./firmware/flasher/flasher_build/obj/*.*
-rm -f ./firmware/mounter/.dep/*.*
-rm -f ./firmware/mounter/mounter_build/lst/*.*
-rm -f ./firmware/mounter/mounter_build/obj/*.*
+
+rm -f ./firmware/*/.dep/*.*
+rm -f ./firmware/*/build/lst/*.*
+rm -f ./firmware/*/build/obj/*.*
+
+rm -f ./firmware/*/*/.dep/*.*
+rm -f ./firmware/*/*/build/lst/*.*
+rm -f ./firmware/*/*/build/obj/*.*
