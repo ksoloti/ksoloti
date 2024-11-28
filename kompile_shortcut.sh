d40ebf3d3d1d9b671c8434d871a5cf0f53713afd
sebiik <sebiik@gmail.com>
2024-11-28 18:39:50 +0800

    remove objdump copmmands (newer chibios will do them automatically)

--- kompile_shortcut.sh
+++ kompile_shortcut.sh
@@ -29,19 +29,9 @@ case "$platform" in
 
         # compile board mode and firmware options
         sh ./platform_osx/compile_firmware.sh BOARD_AXOLOTI_CORE 2>&1 | tee firmware.log
-        # create .list files
-        ./platform_osx/bin/arm-none-eabi-objdump --source-comment --demangle --disassemble ./firmware/flasher/flasher_build/axoloti_flasher.elf > ./firmware/flasher/flasher_build/axoloti_flasher.list
-        ./platform_osx/bin/arm-none-eabi-objdump --source-comment --demangle --disassemble ./firmware/mounter/mounter_build/axoloti_mounter.elf > ./firmware/mounter/mounter_build/axoloti_mounter.list 
-        ./platform_osx/bin/arm-none-eabi-objdump --source-comment --demangle --disassemble ./firmware/build/axoloti.elf > ./firmware/build/axoloti.list
-        ./platform_osx/bin/arm-none-eabi-objdump --source-comment --demangle --disassemble ./firmware/build/axoloti_spilink.elf > ./firmware/build/axoloti_spilink.list
 
         # compile board mode and firmware options
         sh ./platform_osx/compile_firmware.sh BOARD_KSOLOTI_CORE 2>&1 | tee -a firmware.log
-        # create .list files
-        ./platform_osx/bin/arm-none-eabi-objdump --source-comment --demangle --disassemble ./firmware/flasher/flasher_build/ksoloti_flasher.elf > ./firmware/flasher/flasher_build/ksoloti_flasher.list
-        ./platform_osx/bin/arm-none-eabi-objdump --source-comment --demangle --disassemble ./firmware/mounter/mounter_build/ksoloti_mounter.elf > ./firmware/mounter/mounter_build/ksoloti_mounter.list
-        ./platform_osx/bin/arm-none-eabi-objdump --source-comment --demangle --disassemble ./firmware/build/ksoloti.elf > ./firmware/build/ksoloti.list
-        ./platform_osx/bin/arm-none-eabi-objdump --source-comment --demangle --disassemble ./firmware/build/ksoloti_spilink.elf > ./firmware/build/ksoloti_spilink.list
         sh ./qlean.sh
     ;;
     linux)
@@ -50,19 +40,9 @@ case "$platform" in
 
         # compile board mode and firmware options
         sh ./platform_linux/compile_firmware.sh BOARD_AXOLOTI_CORE 2>&1 | tee firmware.log
-        # create .list files
-        ./platform_linux/bin/arm-none-eabi-objdump --source-comment --demangle --disassemble ./firmware/flasher/flasher_build/axoloti_flasher.elf > ./firmware/flasher/flasher_build/axoloti_flasher.list
-        ./platform_linux/bin/arm-none-eabi-objdump --source-comment --demangle --disassemble ./firmware/mounter/mounter_build/axoloti_mounter.elf > ./firmware/mounter/mounter_build/axoloti_mounter.list 
-        ./platform_linux/bin/arm-none-eabi-objdump --source-comment --demangle --disassemble ./firmware/build/axoloti.elf > ./firmware/build/axoloti.list
-        ./platform_linux/bin/arm-none-eabi-objdump --source-comment --demangle --disassemble ./firmware/build/axoloti_spilink.elf > ./firmware/build/axoloti_spilink.list
 
         # compile board mode and firmware options
         sh ./platform_linux/compile_firmware.sh BOARD_KSOLOTI_CORE 2>&1 | tee -a firmware.log
-        # create .list files
-        ./platform_linux/bin/arm-none-eabi-objdump --source-comment --demangle --disassemble ./firmware/flasher/flasher_build/ksoloti_flasher.elf > ./firmware/flasher/flasher_build/ksoloti_flasher.list
-        ./platform_linux/bin/arm-none-eabi-objdump --source-comment --demangle --disassemble ./firmware/mounter/mounter_build/ksoloti_mounter.elf > ./firmware/mounter/mounter_build/ksoloti_mounter.list
-        ./platform_linux/bin/arm-none-eabi-objdump --source-comment --demangle --disassemble ./firmware/build/ksoloti.elf > ./firmware/build/ksoloti.list
-        ./platform_linux/bin/arm-none-eabi-objdump --source-comment --demangle --disassemble ./firmware/build/ksoloti_spilink.elf > ./firmware/build/ksoloti_spilink.list
         sh ./qlean.sh
     ;;
     windows)
@@ -72,19 +52,9 @@ case "$platform" in
 
         # compile board mode and firmware options
         cmd "//C path.bat && compile_firmware.bat BOARD_AXOLOTI_CORE 2>&1 | tee ..\firmware.log"
-        # create .list files
-        cmd "//C path.bat && arm-none-eabi-objdump --source-comment --demangle --disassemble ..\firmware\flasher\flasher_build\axoloti_flasher.elf > ..\firmware\flasher\flasher_build\axoloti_flasher.list"
-        cmd "//C path.bat && arm-none-eabi-objdump --source-comment --demangle --disassemble ..\firmware\mounter\mounter_build\axoloti_mounter.elf > ..\firmware\mounter\mounter_build\axoloti_mounter.list"
-        cmd "//C path.bat && arm-none-eabi-objdump --source-comment --demangle --disassemble ..\firmware\build\axoloti.elf > ..\firmware\build\axoloti.list"
-        cmd "//C path.bat && arm-none-eabi-objdump --source-comment --demangle --disassemble ..\firmware\build\axoloti_spilink.elf > ..\firmware\build\axoloti_spilink.list"
 
         # compile board mode and firmware options
         cmd "//C path.bat && compile_firmware.bat BOARD_KSOLOTI_CORE 2>&1 | tee -a ..\firmware.log"
-        # create .list files
-        cmd "//C path.bat && arm-none-eabi-objdump --source-comment --demangle --disassemble ..\firmware\flasher\flasher_build\ksoloti_flasher.elf > ..\firmware\flasher\flasher_build\ksoloti_flasher.list"
-        cmd "//C path.bat && arm-none-eabi-objdump --source-comment --demangle --disassemble ..\firmware\mounter\mounter_build\ksoloti_mounter.elf > ..\firmware\mounter\mounter_build\ksoloti_mounter.list"
-        cmd "//C path.bat && arm-none-eabi-objdump --source-comment --demangle --disassemble ..\firmware\build\ksoloti.elf > ..\firmware\build\ksoloti.list"
-        cmd "//C path.bat && arm-none-eabi-objdump --source-comment --demangle --disassemble ..\firmware\build\ksoloti_spilink.elf > ..\firmware\build\ksoloti_spilink.list"
 
         cd ..
         sh ./qlean.sh
