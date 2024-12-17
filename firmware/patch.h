8f4724978ae63ac63d3af6dd2aa06040c3b9e92a
sebiik <sebiik@gmail.com>
2024-12-17 23:27:50 +0800

    format

--- firmware/patch.h
+++ firmware/patch.h
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU General Public License along with
  * Axoloti. If not, see <http://www.gnu.org/licenses/>.
  */
+
 #ifndef __PATCH_H
 #define __PATCH_H
 #include <stdint.h>
