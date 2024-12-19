9b61531cdcff887681a451afd223be0c91d5120a
sebiik <sebiik@gmail.com>
2024-12-19 11:34:58 +0800

    reformat serial_midi.c

--- firmware/serial_midi.c
+++ firmware/serial_midi.c
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU General Public License along with
  * Axoloti. If not, see <http://www.gnu.org/licenses/>.
  */
+
 #include "ch.h"
 #include "hal.h"
 #include "axoloti_board.h"
@@ -23,143 +24,157 @@
 #include "serial_midi.h"
 #include "patch.h"
 
-static unsigned char StatusLengthLookup[16] = {0, 0, 0, 0, 0, 0, 0, 0, 3, // 0x80=note off, 3 bytes
-                                               3, // 0x90=note on, 3 bytes
-                                               3, // 0xa0=poly pressure, 3 bytes
-                                               3, // 0xb0=control change, 3 bytes
-                                               2, // 0xc0=program change, 2 bytes
-                                               2, // 0xd0=channel pressure, 2 bytes
-                                               3, // 0xe0=pitch bend, 3 bytes
-                                               -1 // 0xf0=other things. may vary.
-    };
+static const uint8_t StatusLengthLookup[16] = { //TODO should be int8_t
+    0, 0, 0, 0, 0, 0, 0, 0,
+    3, /* 0x80 = note off, 3 bytes */
+    3, /* 0x90 = note on, 3 bytes */
+    3, /* 0xA0 = poly pressure, 3 bytes */
+    3, /* 0xB0 = control change, 3 bytes */
+    2, /* 0xC0 = program change, 2 bytes */
+    2, /* 0xD0 = channel pressure, 2 bytes */
+    3, /* 0xE0 = pitch bend, 3 bytes */
+    -1 /* 0xF0 = other things. may vary */
+};
 
-const signed char SysMsgLengthLookup[16] = {-1, // 0xf0=sysex start. may vary
-    2, // 0xf1=MIDI Time Code. 2 bytes
-    3, // 0xf2=MIDI Song position. 3 bytes
-    2, // 0xf3=MIDI Song Select. 2 bytes.
-    1, // 0xf4=undefined
-    1, // 0xf5=undefined
-    1, // 0xf6=TUNE Request
-    1, // 0xf7=sysex end.
-    1, // 0xf8=timing clock. 1 byte
-    1, // 0xf9=proposed measure end?
-    1, // 0xfa=start. 1 byte
-    1, // 0xfb=continue. 1 byte
-    1, // 0xfc=stop. 1 byte
-    1, // 0xfd=undefined
-    1, // 0xfe=active sensing. 1 byte
-    3 // 0xff= not reset, but a META-EVENT, which is always 3 bytes
-    };
+static const int8_t SysMsgLengthLookup[16] = {
+    -1, /* 0xF0 = sysex start, may vary */
+    2,  /* 0xF1 = MIDI Time Code, 2 bytes */
+    3,  /* 0xF2 = MIDI Song position, 3 bytes */
+    2,  /* 0xF3 = MIDI Song Select, 2 bytes. */
+    1,  /* 0xF4 = undefined */
+    1,  /* 0xF5 = undefined */
+    1,  /* 0xF6 = TUNE Request */
+    1,  /* 0xF7 = sysex end */
+    1,  /* 0xF8 = timing clock 1 byte */
+    1,  /* 0xF9 = proposed measure end? */
+    1,  /* 0xFA = start 1 byte */
+    1,  /* 0xFB = continue 1 byte */
+    1,  /* 0xFC = stop 1 byte */
+    1,  /* 0xFD = undefined */
+    1,  /* 0xFE = active sensing 1 byte */
+    3   /* 0xFF = not reset, but a META-EVENT, which is always 3 bytes */
+};
 
-unsigned char MidiByte0;
-unsigned char MidiByte1;
-unsigned char MidiByte2;
-unsigned char MidiCurData;
-unsigned char MidiNumData;
-unsigned char MidiInChannel;
+static const SerialConfig sdMidiCfg = {
+    31250, /* baud */
+    0, 0, 0
+};
 
-void serial_MidiInByteHandler(uint8_t data);
+uint8_t MidiByte0, MidiByte1, MidiByte2;
+uint8_t MidiCurData;
+uint8_t MidiNumData;
+uint8_t MidiInChannel;
 
+static WORKING_AREA(waThreadMidi, 256) __attribute__((section (".ccmramend")));
+
+
+/* MIDI IN */
 
 void serial_MidiInByteHandler(uint8_t data) {
-  int8_t len;
-  if (data & 0x80) {
-    len = StatusLengthLookup[data >> 4];
-    if (len == -1) {
-      len = SysMsgLengthLookup[data - 0xF0];
-      if (len == 1) {
-        MidiInMsgHandler(MIDI_DEVICE_DIN, 1, data, 0, 0);
-      }
-      else {
-        MidiByte0 = data;
-        MidiNumData = len - 1;
-        MidiCurData = 0;
-      }
+    int8_t len;
+
+    if (data & 0x80) {
+        len = StatusLengthLookup[data >> 4];
+
+        if (len == -1) {
+            len = SysMsgLengthLookup[data - 0xF0];
+
+            if (len == 1) {
+                MidiInMsgHandler(MIDI_DEVICE_DIN, 1, data, 0, 0);
+            }
+            else {
+                MidiByte0 = data;
+                MidiNumData = len - 1;
+                MidiCurData = 0;
+            }
+        }
+        else {
+            MidiByte0 = data;
+            MidiNumData = len - 1;
+            MidiCurData = 0;
+        }
     }
     else {
-      MidiByte0 = data;
-      MidiNumData = len - 1;
-      MidiCurData = 0;
+        /* not a status byte */
+
+        if (MidiCurData == 0) {
+            MidiByte1 = data;
+
+            if (MidiNumData == 1) {
+                /* 2 byte message complete */
+                MidiInMsgHandler(MIDI_DEVICE_DIN, 1, MidiByte0, MidiByte1, 0);
+                MidiCurData = 0;
+            }
+            else {
+                MidiCurData++;
+            }
+        }
+        else if (MidiCurData == 1) {
+            MidiByte2 = data;
+
+            if (MidiNumData == 2) {
+                MidiInMsgHandler(MIDI_DEVICE_DIN, 1, MidiByte0, MidiByte1, MidiByte2);
+                MidiCurData = 0;
+            }
+        }
     }
-  }
-  else // not a status byte
-  {
-    if (MidiCurData == 0) {
-      MidiByte1 = data;
-      if (MidiNumData == 1) {
-        // 2 byte message complete
-        MidiInMsgHandler(MIDI_DEVICE_DIN, 1, MidiByte0, MidiByte1, 0);
-        MidiCurData = 0;
-      }
-      else
-        MidiCurData++;
-    }
-    else if (MidiCurData == 1) {
-      MidiByte2 = data;
-      if (MidiNumData == 2) {
-        MidiInMsgHandler(MIDI_DEVICE_DIN, 1, MidiByte0, MidiByte1, MidiByte2);
-        MidiCurData = 0;
-      }
-    }
-  }
 }
 
-// Midi OUT
+
+/* MIDI OUT */
 
 void serial_MidiSend1(uint8_t b0) {
-  sdPut(&SDMIDI, b0);
+    sdPut(&SDMIDI, b0);
 }
+
 
 void serial_MidiSend2(uint8_t b0, uint8_t b1) {
-  unsigned char tx[2];
-  tx[0] = b0;
-  tx[1] = b1;
-  sdWrite(&SDMIDI, tx, 2);
+    uint8_t tx[2];
+    tx[0] = b0;
+    tx[1] = b1;
+    sdWrite(&SDMIDI, tx, 2);
 }
+
 
 void serial_MidiSend3(uint8_t b0, uint8_t b1, uint8_t b2) {
-  unsigned char tx[3];
-  tx[0] = b0;
-  tx[1] = b1;
-  tx[2] = b2;
-  sdWrite(&SDMIDI, tx, 3);
+    uint8_t tx[3];
+    tx[0] = b0;
+    tx[1] = b1;
+    tx[2] = b2;
+    sdWrite(&SDMIDI, tx, 3);
 }
 
-int serial_MidiGetOutputBufferPending(void) {
-  return chOQGetFullI(&SDMIDI.oqueue);
+
+int32_t serial_MidiGetOutputBufferPending(void) {
+    return chOQGetFullI(&SDMIDI.oqueue);
 }
 
-// Midi UART...
-static const SerialConfig sdMidiCfg = {31250, // baud
-    0, 0, 0};
 
-static WORKING_AREA(waThreadMidi, 256) __attribute__ ((section (".ccmramend")));
+/* MIDI UART */
 
-__attribute__((noreturn))
-  static msg_t ThreadMidi(void *arg) {
-  (void)arg;
+__attribute__((noreturn)) static msg_t ThreadMidi(void *arg) {
+    (void)arg;
 #if CH_CFG_USE_REGISTRY
-  chRegSetThreadName("midi");
+    chRegSetThreadName("midi");
 #endif
-  while (1) {
-    char ch;
-    ch = sdGet(&SDMIDI);
-    serial_MidiInByteHandler(ch);
-  }
+    while (1) {
+        char ch;
+        ch = sdGet(&SDMIDI);
+        serial_MidiInByteHandler(ch);
+    }
 }
+
 
 void serial_midi_init(void) {
-  /*
-   * Activates the serial driver 2 using the driver default configuration.
-   * PA2(TX) and PA3(RX) are routed to USART2.
-   */
-  // RX
-  palSetPadMode(GPIOG, 9, PAL_MODE_ALTERNATE(8) | PAL_MODE_INPUT_PULLUP);
-  // TX
-  // palSetPadMode(GPIOG, 14, PAL_MODE_ALTERNATE(8) | PAL_STM32_OTYPE_OPENDRAIN); // seb don't know why but TX didn't work in open drain
-   palSetPadMode(GPIOG, 14, PAL_MODE_ALTERNATE(8) | PAL_STM32_PUDR_PULLUP); // seb works so far
-  // palSetPadMode(GPIOG, 14, PAL_MODE_ALTERNATE(8) | PAL_MODE_OUTPUT_PUSHPULL); // does not seem to work with some MIDI devices
+    /*
+     * Activates the serial driver 2 using the driver default configuration.
+     * PA2(TX) and PA3(RX) are routed to USART2.
+     */
 
-  sdStart(&SDMIDI, &sdMidiCfg);
-  chThdCreateStatic(waThreadMidi, sizeof(waThreadMidi), NORMALPRIO, (void*) ThreadMidi, NULL);
+    palSetPadMode(GPIOG,  9, PAL_MODE_ALTERNATE(8) | PAL_MODE_INPUT_PULLUP); // RX
+    palSetPadMode(GPIOG, 14, PAL_MODE_ALTERNATE(8) | PAL_MODE_INPUT_PULLUP); // TX // TODO may add PAL_MODE_OUTPUT_PUSHPULL
+
+    sdStart(&SDMIDI, &sdMidiCfg);
+
+    chThdCreateStatic(waThreadMidi, sizeof(waThreadMidi), NORMALPRIO, (void*) ThreadMidi, NULL);
 }
