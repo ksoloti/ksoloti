fa4ea0edac7161352710ba9ad497e81178ffae28
sebiik <sebiik@gmail.com>
2025-01-13 19:30:07 +0800

    "spaces in path" fix, reformat

--- firmware/Makefile.patch.mk
+++ firmware/Makefile.patch.mk
@@ -2,52 +2,49 @@ BOARDDEF =
 FWOPTIONDEF =
 
 
-# Some new options are important to keep
-# SRAM usage and DSP load low with newer GCC versions.
+# Some new options are important to keep SRAM usage and DSP load low with newer GCC versions.
 # "--param max-completely-peeled-insns=100" makes a big difference to get SRAM down. Newer GCC versions use 200 here, original axoloti (GCC 4.9) used 100.
-# below the single backslash line are options which are unknown to make any difference so far
+# Added a few flags that speed up floating-point calculation at the expense of precision. Graciously shared by https://github.com/malyzajko/daisy/blob/master/doc/documentation.md#running-generated-code
 CCFLAGS = \
-    -Wno-implicit-fallthrough \
-    -Wno-unused-parameter \
-    -Wno-return-type \
-    -ggdb3 \
-    -mcpu=cortex-m4 \
-    -mfloat-abi=hard \
-    -mfpu=fpv4-sp-d16 \
-    -mthumb \
-    -mtune=cortex-m4 \
-    -mword-relocations \
-    -nostartfiles \
-    -nostdlib \
-    -std=c++11 \
-    -O3 \
-    --param max-completely-peeled-insns=100 \
-    -fcode-hoisting \
-    -fno-threadsafe-statics \
-    -ffunction-sections \
-    -fdata-sections \
-    -fno-common \
-    -fno-math-errno \
-    -fno-reorder-blocks \
-    -fno-rtti \
-    -mno-thumb-interwork \
-    -fno-use-cxa-atexit \
-    -fpermissive \
-    -ffast-math \
-    -fno-unsafe-math-optimizations \
-    -fno-signed-zeros \
-    -ffp-contract=off \
-    \
-
+  -Wno-implicit-fallthrough \
+  -Wno-unused-parameter \
+  -Wno-return-type \
+  -ggdb3 \
+  -mcpu=cortex-m4 \
+  -mfloat-abi=hard \
+  -mfpu=fpv4-sp-d16 \
+  -mthumb \
+  -mtune=cortex-m4 \
+  -mword-relocations \
+  -nostartfiles \
+  -nostdlib \
+  -std=c++11 \
+  -O3 \
+  --param max-completely-peeled-insns=100 \
+  -fcode-hoisting \
+  -fno-threadsafe-statics \
+  -ffunction-sections \
+  -fdata-sections \
+  -fno-common \
+  -fno-math-errno \
+  -fno-reorder-blocks \
+  -fno-rtti \
+  -mno-thumb-interwork \
+  -fno-use-cxa-atexit \
+  -fpermissive \
+  -ffast-math \
+  -fno-unsafe-math-optimizations \
+  -fno-signed-zeros \
+  -ffp-contract=off
 
 DEFS = \
-    -D$(BOARDDEF) \
-    -DARM_MATH_CM4 \
-    -DCORTEX_USE_FPU=TRUE \
-    -DTHUMB \
-    -DTHUMB_NO_INTERWORKING \
-    -DTHUMB_PRESENT \
-    -D__FPU_PRESENT
+  -D$(BOARDDEF) \
+  -DARM_MATH_CM4 \
+  -DCORTEX_USE_FPU=TRUE \
+  -DTHUMB \
+  -DTHUMB_NO_INTERWORKING \
+  -DTHUMB_PRESENT \
+  -D__FPU_PRESENT
 
 ifneq ($(FWOPTIONDEF),)
   DEFS := $(DEFS) -D$(FWOPTIONDEF)
@@ -72,40 +69,43 @@ ifeq ($(FWOPTIONDEF),FW_USBAUDIO)
 endif
 
 LDFLAGS = \
-    $(RAMLINKOPT) \
-    -Bsymbolic \
-    -Wl,--gc-sections \
-    -Wl,--print-memory-usage \
-    -fno-common \
-    -mcpu=cortex-m4 \
-    -mfloat-abi=hard \
-    -mfpu=fpv4-sp-d16 \
-    -mno-thumb-interwork \
-    -mthumb \
-    -mtune=cortex-m4 \
-    -nostartfiles
+  $(RAMLINKOPT) \
+  -Bsymbolic \
+  -Wl,--gc-sections \
+  -Wl,--print-memory-usage \
+  -fno-common \
+  -mcpu=cortex-m4 \
+  -mfloat-abi=hard \
+  -mfpu=fpv4-sp-d16 \
+  -mno-thumb-interwork \
+  -mthumb \
+  -mtune=cortex-m4 \
+  -nostartfiles
+
+DMPFLAGS = \
+  --demangle \
+  --disassemble \
+  --source-comment
 
 TRGT = arm-none-eabi-
-CC=$(TRGT)gcc
-CPP=$(TRGT)g++
-LD=$(TRGT)gcc
-CP=$(TRGT)objcopy
-DMP=$(TRGT)objdump
-SIZ=$(TRGT)size
+CC   = $(TRGT)gcc
+CPP  = $(TRGT)g++
+LD   = $(TRGT)gcc
+CP   = $(TRGT)objcopy
+DMP  = $(TRGT)objdump
+SIZ  = $(TRGT)size
 
-axoloti_runtime ?= ..
-axoloti_release ?= ..
-axoloti_home ?= ..
 axoloti_libraries ?= ..
-axoloti_firmware ?= ../firmware
-
-CHIBIOS = ${axoloti_release}/chibios
-CMSIS = ${axoloti_release}/CMSIS
+axoloti_firmware  ?= ../firmware
+axoloti_home      ?= ..
 
 EMPTY := 
 SPACE := $(EMPTY) $(EMPTY)
-BUILDDIR=$(subst $(SPACE),\ ,${axoloti_libraries}/build)
-FIRMWARE=$(subst $(SPACE),\ ,${axoloti_firmware})
+
+BUILDDIR = $(subst $(SPACE),\ ,${axoloti_libraries}/build)
+FIRMWARE = $(subst $(SPACE),\ ,${axoloti_firmware})
+CHIBIOS  = $(subst $(SPACE),\ ,${axoloti_home}/chibios)
+CMSIS    = $(subst $(SPACE),\ ,${axoloti_home}/CMSIS)
 
 
 # Startup files.
@@ -122,39 +122,20 @@ include $(CHIBIOS)/os/various/fatfs_bindings/fatfs.mk
 
 
 INCDIR = $(CMSIS)/Core/Include \
-	 $(CMSIS)/DSP/Include \
-         $(PORTINC) $(KERNINC) $(TESTINC) \
-         $(HALINC) $(PLATFORMINC) $(BOARDINC) \
-	 $(FATFSINC) \
-         $(OSALINC) \
-         ${FIRMWARE} \
-	 $(CHIBIOS) \
-         $(CHIBIOS)/os/various \
-         ${FIRMWARE}/STM32F4xx_HAL_Driver/Inc \
-	 ${FIRMWARE}/mutable_instruments \
-	 $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC \
-         $(CHIBIOS)/os/common/ports/ARMCMx/devices/STM32F4xx \
-         $(CHIBIOS)/os/ext/CMSIS/include \
-         $(CHIBIOS)/os/ext/CMSIS/ST/STM32F4xx
-
-# INCDIR = ${CMSIS}/Core/Include \
-#          ${CMSIS}/DSP/Include \
-#          $(STARTUPINC) \
-#          $(PORTINC) $(KERNINC) $(TESTINC) \
-#          $(HALINC) $(PLATFORMINC) $(BOARDINC) \
-#          $(FATFSINC) \
-#          $(OSALINC) \
-#          $(CHIBIOS)/os/various \
-#          ${FIRMWARE}/STM32_USB_Host_Library/Core/Inc \
-#          ${FIRMWARE}/STM32_USB_Host_Library/Class/HID/Inc \
-#          ${FIRMWARE}/STM32F4xx_HAL_Driver/Inc \
-#          ${MUTABLE_INSTRUMENTS} \
-#          ${MUTABLE_INSTRUMENTS}/warps \
-#          ${MUTABLE_INSTRUMENTS}/braids \
-#          ${MUTABLE_INSTRUMENTS}/rings \
-#          ${MUTABLE_INSTRUMENTS}/elements \
-#          ${MUTABLE_INSTRUMENTS}/stmlib \
-# 	     $(CHIBIOS)/os/hal/lib/streams
+  $(CMSIS)/DSP/Include \
+  $(PORTINC) $(KERNINC) $(TESTINC) \
+  $(HALINC) $(PLATFORMINC) $(BOARDINC) \
+  $(FATFSINC) \
+  $(OSALINC) \
+  ${FIRMWARE} \
+  $(CHIBIOS) \
+  $(CHIBIOS)/os/various \
+  ${FIRMWARE}/STM32F4xx_HAL_Driver/Inc \
+  ${FIRMWARE}/mutable_instruments \
+  $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC \
+  $(CHIBIOS)/os/common/ports/ARMCMx/devices/STM32F4xx \
+  $(CHIBIOS)/os/ext/CMSIS/include \
+  $(CHIBIOS)/os/ext/CMSIS/ST/STM32F4xx
 
 # Paths
 IINCDIR = $(patsubst %,-I%,$(INCDIR) $(DINCDIR) $(UINCDIR))
@@ -182,10 +163,9 @@ ${BUILDDIR}/xpatch.bin: ${BUILDDIR}/xpatch.cpp ${BUILDDIR}/xpatch.h.gch
 #	@$(SIZ) --format=berkeley ${BUILDDIR}/xpatch.elf >> ${BUILDDIR}/xpatch.siz
 
 #	@echo Creating LST file for debugging
-	@$(DMP) --demangle --source-comment --disassemble ${BUILDDIR}/xpatch.elf > ${BUILDDIR}/xpatch.list
-#   (--source-comment now supported in gcc9) --line-numbers 
-
-.PHONY: clean
+	@$(DMP) $(DMPFLAGS) ${BUILDDIR}/xpatch.elf > ${BUILDDIR}/xpatch.lst
 
 clean:
 	@rm -f ${BUILDDIR}/xpatch.o ${BUILDDIR}/xpatch.elf ${BUILDDIR}/xpatch.bin ${BUILDDIR}/xpatch.d ${BUILDDIR}/xpatch.map ${BUILDDIR}/xpatch.list ${BUILDDIR}/xpatch.h.gch
+
+.PHONY: all clean
