ebece0915c96c465a5a8367c2361f7da76bf634a
sebiik <sebiik@gmail.com>
2025-01-28 14:16:20 +0800

    windows dropbox ignores

--- dropbox_ignore.ps1
+++ dropbox_ignore.ps1
@@ -1,4 +1,6 @@
 Set-Content -Path 'C:\Users\sbcw\Dropbox\pcb-layouts\axo-427-core\ksoloti\build' -Stream com.dropbox.ignored -Value 1 -verbose
+Set-Content -Path 'C:\Users\sbcw\Dropbox\pcb-layouts\axo-427-core\ksoloti\dist' -Stream com.dropbox.ignored -Value 1 -verbose
+Set-Content -Path 'C:\Users\sbcw\Dropbox\pcb-layouts\axo-427-core\ksoloti\packagetemp' -Stream com.dropbox.ignored -Value 1 -verbose
 Set-Content -Path 'C:\Users\sbcw\Dropbox\pcb-layouts\axo-427-core\ksoloti\firmware\.dep' -Stream com.dropbox.ignored -Value 1 -verbose
 Set-Content -Path 'C:\Users\sbcw\Dropbox\pcb-layouts\axo-427-core\ksoloti\firmware\build' -Stream com.dropbox.ignored -Value 1 -verbose
 Set-Content -Path 'C:\Users\sbcw\Dropbox\pcb-layouts\axo-427-core\ksoloti\firmware\flasher\.dep' -Stream com.dropbox.ignored -Value 1 -verbose
