22e32246dfa82c4c271401aca26b77eff44bf67b
sebiik <sebiik@gmail.com>
2025-02-11 15:12:05 +0800

    fast8

--- firmware/patch.c
+++ firmware/patch.c
@@ -560,7 +560,7 @@ void start_dsp_thread(void) {
 
 
 void computebufI(int32_t* inp, int32_t* outp) {
-    uint8_t i; for (i = 0; i < 32; i++) {
+    uint_fast8_t i; for (i = 0; i < 32; i++) {
         inbuf[i] = inp[i];
     }
 
