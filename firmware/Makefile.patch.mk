e65808bd8bb4b81c5d0d85fb455786bee15dfe0c
AndrewCapon <andrew.capon@zen.co.uk>
2025-03-03 08:07:22 +0000

    Add some debug info for patcher make

--- firmware/Makefile.patch.mk
+++ firmware/Makefile.patch.mk
@@ -3,7 +3,9 @@ SUBBOARDDEF=
 FWOPTIONDEF =
 LINKERFILE =
 
-$(info Patcher Compiling for $(SUBBOARDDEF) using linker file $(LINKERFILE))
+$(info SUBBOARDDEF = $(SUBBOARDDEF))
+$(info FWOPTIONDEF = $(FWOPTIONDEF))
+$(info LINKERFILE  = $(LINKERFILE))
 
 # Some new options are important to keep SRAM usage and DSP load low with newer GCC versions.
 # "--param max-completely-peeled-insns=100" makes a big difference to get SRAM down. Newer GCC versions use 200 here, original axoloti (GCC 4.9) used 100.
