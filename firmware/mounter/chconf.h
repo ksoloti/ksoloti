9268cc593069c31360f8a5daac80478128a36aff
sebiik <sebiik@gmail.com>
2025-04-22 22:33:04 +0800

    Revert "mounter chconf? likely won't make a difference"
    This reverts commit 9fed8883d66d454dd660ed82b59129298662aac3.

--- firmware/mounter/chconf.h
+++ firmware/mounter/chconf.h
@@ -291,7 +291,7 @@
  * @note    The default is @p TRUE.
  */
 #if !defined(CH_CFG_USE_MEMCORE) || defined(__DOXYGEN__)
-#define CH_CFG_USE_MEMCORE                  FALSE
+#define CH_CFG_USE_MEMCORE                  TRUE
 #endif
 
 /**
@@ -423,7 +423,7 @@
  * @note    The default is @p FALSE.
  */
 #if !defined(CH_DBG_FILL_THREADS) || defined(__DOXYGEN__)
-#define CH_DBG_FILL_THREADS             FALSE
+#define CH_DBG_FILL_THREADS             TRUE
 #endif
 
 /**
