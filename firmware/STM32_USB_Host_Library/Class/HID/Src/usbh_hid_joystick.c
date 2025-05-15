9b28254b606e08f2e4cdf2ce8d34c200db0ebe45
sebiik <sebiik@gmail.com>
2025-05-15 10:40:16 +0800

    add original ST license info

--- firmware/STM32_USB_Host_Library/Class/HID/Src/usbh_hid_joystick.c
+++ firmware/STM32_USB_Host_Library/Class/HID/Src/usbh_hid_joystick.c
@@ -8,6 +8,25 @@
  * https://blog.csdn.net/softlove03/article/details/128616152
  * https://blog.csdn.net/softlove03/article/details/128646031
  *
+ *
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
+ *
+ * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *        http://www.st.com/software_license_agreement_liberty_v2
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************
  */
 
 
