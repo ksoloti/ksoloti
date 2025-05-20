c39131e90b213275b2cc4f872c4692243705b65f
sebiik <sebiik@gmail.com>
2025-05-20 18:20:17 +0800

    use PLL2_P (75MHz) to create SPI123 clock

--- firmware/mcuconf.h
+++ firmware/mcuconf.h
@@ -195,7 +195,7 @@
 #define STM32_DFSDM1SEL                     STM32_DFSDM1SEL_PCLK2
 #define STM32_SPDIFSEL                      STM32_SPDIFSEL_PLL1_Q_CK
 #define STM32_SPI45SEL                      STM32_SPI45SEL_PCLK2
-#define STM32_SPI123SEL                     STM32_SPI123SEL_PLL1_Q_CK
+#define STM32_SPI123SEL                     STM32_SPI123SEL_PLL2_P_CK
 #define STM32_SAI23SEL                      STM32_SAI23SEL_PLL1_Q_CK
 #define STM32_SAI1SEL                       STM32_SAI1SEL_PLL1_Q_CK
 #define STM32_LPTIM1SEL                     STM32_LPTIM1SEL_PCLK1
