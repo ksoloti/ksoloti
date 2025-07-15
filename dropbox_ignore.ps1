ae407a9eeccce235f6dbe0cdbf48c160ecb08ab9
sebiik <sebiik@gmail.com>
2025-07-15 21:26:58 +0800

    add zulu* folders to .gitignore

--- dropbox_ignore.ps1
+++
@@ -1,7 +0,0 @@
-Set-Content -Path 'C:\Users\sbcw\Dropbox\pcb-layouts\axo-427-core\ksoloti\build' -Stream com.dropbox.ignored -Value 1 -verbose
-Set-Content -Path 'C:\Users\sbcw\Dropbox\pcb-layouts\axo-427-core\ksoloti\dist' -Stream com.dropbox.ignored -Value 1 -verbose
-Set-Content -Path 'C:\Users\sbcw\Dropbox\pcb-layouts\axo-427-core\ksoloti\packagetemp' -Stream com.dropbox.ignored -Value 1 -verbose
-Set-Content -Path 'C:\Users\sbcw\Dropbox\pcb-layouts\axo-427-core\ksoloti\firmware\build' -Stream com.dropbox.ignored -Value 1 -verbose
-Set-Content -Path 'C:\Users\sbcw\Dropbox\pcb-layouts\axo-427-core\ksoloti\firmware\flasher\flasher_build' -Stream com.dropbox.ignored -Value 1 -verbose
-Set-Content -Path 'C:\Users\sbcw\Dropbox\pcb-layouts\axo-427-core\ksoloti\firmware\mounter\mounter_build' -Stream com.dropbox.ignored -Value 1 -verbose
-$null = Read-Host 'Press Enter to close this window...'
