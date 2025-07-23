0c661d7d994feeeb4320d37622140101d34a83d4
sebiik <sebiik@gmail.com>
2025-07-23 12:17:06 +0800

    Reduce timeout from 10s to 6s

--- src/main/java/axoloti/Patch.java
+++ src/main/java/axoloti/Patch.java
@@ -346,7 +346,7 @@ public class Patch {
 
     public boolean waitForBinFile() {
         boolean fileFound = false;
-        for (int i = 0; i < 200; i++) {
+        for (int i = 0; i < 120; i++) {
             if (this.getBinFile().exists()) {
                 fileFound = true;
                 return fileFound;
