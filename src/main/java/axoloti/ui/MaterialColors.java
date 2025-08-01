ec23941418956dec3db988d02906f598ecc54cf9
sebiik <sebiik@gmail.com>
2025-08-01 22:08:44 +0800

    Introduce MaterialColors color list for easier styilng

---
+++ src/main/java/axoloti/ui/MaterialColors.java
@@ -0,0 +1,286 @@
+/**
+ * Copyright (C) 2013, 2014, 2015 Johannes Taelman
+ * Edited 2023 - 2024 by Ksoloti
+ *
+ * This file is part of Axoloti.
+ *
+ * Axoloti is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Axoloti is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Axoloti. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package axoloti.ui;
+
+import java.awt.Color;
+
+/**
+ * A utility class providing Color constants for the Material Design color palette.
+ * Color definitions from https://materialui.co/colors
+ * @author Ksoloti
+ */
+public final class MaterialColors {
+
+    // Red
+    public static final Color RED_50 = new Color(0xFFEBEE);
+    public static final Color RED_100 = new Color(0xFFCDD2);
+    public static final Color RED_200 = new Color(0xEF9A9A);
+    public static final Color RED_300 = new Color(0xE57373);
+    public static final Color RED_400 = new Color(0xEF5350);
+    public static final Color RED_500 = new Color(0xF44336);
+    public static final Color RED_600 = new Color(0xE53935);
+    public static final Color RED_700 = new Color(0xD32F2F);
+    public static final Color RED_800 = new Color(0xC62828);
+    public static final Color RED_900 = new Color(0xB71C1C);
+
+    // Pink
+    public static final Color PINK_50 = new Color(0xFCE4EC);
+    public static final Color PINK_100 = new Color(0xF8BBD0);
+    public static final Color PINK_200 = new Color(0xF48FB1);
+    public static final Color PINK_300 = new Color(0xF06292);
+    public static final Color PINK_400 = new Color(0xEC407A);
+    public static final Color PINK_500 = new Color(0xE91E63);
+    public static final Color PINK_600 = new Color(0xD81B60);
+    public static final Color PINK_700 = new Color(0xC2185B);
+    public static final Color PINK_800 = new Color(0xAD1457);
+    public static final Color PINK_900 = new Color(0x880E4F);
+
+    // Purple
+    public static final Color PURPLE_50 = new Color(0xF3E5F5);
+    public static final Color PURPLE_100 = new Color(0xE1BEE7);
+    public static final Color PURPLE_200 = new Color(0xCE93D8);
+    public static final Color PURPLE_300 = new Color(0xBA68C8);
+    public static final Color PURPLE_400 = new Color(0xAB47BC);
+    public static final Color PURPLE_500 = new Color(0x9C27B0);
+    public static final Color PURPLE_600 = new Color(0x8E24AA);
+    public static final Color PURPLE_700 = new Color(0x7B1FA2);
+    public static final Color PURPLE_800 = new Color(0x6A1B9A);
+    public static final Color PURPLE_900 = new Color(0x4A148C);
+
+    // Deep Purple
+    public static final Color DEEP_PURPLE_50 = new Color(0xEDE7F6);
+    public static final Color DEEP_PURPLE_100 = new Color(0xD1C4E9);
+    public static final Color DEEP_PURPLE_200 = new Color(0xB39DDB);
+    public static final Color DEEP_PURPLE_300 = new Color(0x9575CD);
+    public static final Color DEEP_PURPLE_400 = new Color(0x7E57C2);
+    public static final Color DEEP_PURPLE_500 = new Color(0x673AB7);
+    public static final Color DEEP_PURPLE_600 = new Color(0x5E35B1);
+    public static final Color DEEP_PURPLE_700 = new Color(0x512DA8);
+    public static final Color DEEP_PURPLE_800 = new Color(0x4527A0);
+    public static final Color DEEP_PURPLE_900 = new Color(0x311B92);
+
+    // Indigo
+    public static final Color INDIGO_50 = new Color(0xE8EAF6);
+    public static final Color INDIGO_100 = new Color(0xC5CAE9);
+    public static final Color INDIGO_200 = new Color(0x9FA8DA);
+    public static final Color INDIGO_300 = new Color(0x7986CB);
+    public static final Color INDIGO_400 = new Color(0x5C6BC0);
+    public static final Color INDIGO_500 = new Color(0x3F51B5);
+    public static final Color INDIGO_600 = new Color(0x3949AB);
+    public static final Color INDIGO_700 = new Color(0x303F9F);
+    public static final Color INDIGO_800 = new Color(0x283593);
+    public static final Color INDIGO_900 = new Color(0x1A237E);
+
+    // Blue
+    public static final Color BLUE_50 = new Color(0xE3F2FD);
+    public static final Color BLUE_100 = new Color(0xBBDEFB);
+    public static final Color BLUE_200 = new Color(0x90CAF9);
+    public static final Color BLUE_300 = new Color(0x64B5F6);
+    public static final Color BLUE_400 = new Color(0x42A5F5);
+    public static final Color BLUE_500 = new Color(0x2196F3);
+    public static final Color BLUE_600 = new Color(0x1E88E5);
+    public static final Color BLUE_700 = new Color(0x1976D2);
+    public static final Color BLUE_800 = new Color(0x1565C0);
+    public static final Color BLUE_900 = new Color(0x0D47A1);
+
+    // Light Blue
+    public static final Color LIGHT_BLUE_50 = new Color(0xE1F5FE);
+    public static final Color LIGHT_BLUE_100 = new Color(0xB3E5FC);
+    public static final Color LIGHT_BLUE_200 = new Color(0x81D4FA);
+    public static final Color LIGHT_BLUE_300 = new Color(0x4FC3F7);
+    public static final Color LIGHT_BLUE_400 = new Color(0x29B6F6);
+    public static final Color LIGHT_BLUE_500 = new Color(0x03A9F4);
+    public static final Color LIGHT_BLUE_600 = new Color(0x039BE5);
+    public static final Color LIGHT_BLUE_700 = new Color(0x0288D1);
+    public static final Color LIGHT_BLUE_800 = new Color(0x0277BD);
+    public static final Color LIGHT_BLUE_900 = new Color(0x01579B);
+
+    // Cyan
+    public static final Color CYAN_50 = new Color(0xE0F7FA);
+    public static final Color CYAN_100 = new Color(0xB2EBF2);
+    public static final Color CYAN_200 = new Color(0x80DEEA);
+    public static final Color CYAN_300 = new Color(0x4DD0E1);
+    public static final Color CYAN_400 = new Color(0x26C6DA);
+    public static final Color CYAN_500 = new Color(0x00BCD4);
+    public static final Color CYAN_600 = new Color(0x00ACC1);
+    public static final Color CYAN_700 = new Color(0x0097A7);
+    public static final Color CYAN_800 = new Color(0x00838F);
+    public static final Color CYAN_900 = new Color(0x006064);
+
+    // Teal
+    public static final Color TEAL_50 = new Color(0xE0F2F1);
+    public static final Color TEAL_100 = new Color(0xB2DFDB);
+    public static final Color TEAL_200 = new Color(0x80CBC4);
+    public static final Color TEAL_300 = new Color(0x4DB6AC);
+    public static final Color TEAL_400 = new Color(0x26A69A);
+    public static final Color TEAL_500 = new Color(0x009688);
+    public static final Color TEAL_600 = new Color(0x00897B);
+    public static final Color TEAL_700 = new Color(0x00796B);
+    public static final Color TEAL_800 = new Color(0x00695C);
+    public static final Color TEAL_900 = new Color(0x004D40);
+
+    // Green
+    public static final Color GREEN_50 = new Color(0xE8F5E9);
+    public static final Color GREEN_100 = new Color(0xC8E6C9);
+    public static final Color GREEN_200 = new Color(0xA5D6A7);
+    public static final Color GREEN_300 = new Color(0x81C784);
+    public static final Color GREEN_400 = new Color(0x66BB6A);
+    public static final Color GREEN_500 = new Color(0x4CAF50);
+    public static final Color GREEN_600 = new Color(0x43A047);
+    public static final Color GREEN_700 = new Color(0x388E3C);
+    public static final Color GREEN_800 = new Color(0x2E7D32);
+    public static final Color GREEN_900 = new Color(0x1B5E20);
+
+    // Light Green
+    public static final Color LIGHT_GREEN_50 = new Color(0xF7F9F0);
+    public static final Color LIGHT_GREEN_100 = new Color(0xF1F8E9);
+    public static final Color LIGHT_GREEN_200 = new Color(0xC5E1A5);
+    public static final Color LIGHT_GREEN_300 = new Color(0xAED581);
+    public static final Color LIGHT_GREEN_400 = new Color(0x9CCC65);
+    public static final Color LIGHT_GREEN_500 = new Color(0x8BC34A);
+    public static final Color LIGHT_GREEN_600 = new Color(0x7CB342);
+    public static final Color LIGHT_GREEN_700 = new Color(0x689F38);
+    public static final Color LIGHT_GREEN_800 = new Color(0x558B2F);
+    public static final Color LIGHT_GREEN_900 = new Color(0x33691E);
+
+    // Lime
+    public static final Color LIME_50 = new Color(0xF9FBE7);
+    public static final Color LIME_100 = new Color(0xF0F4C3);
+    public static final Color LIME_200 = new Color(0xE6EE9C);
+    public static final Color LIME_300 = new Color(0xDCE775);
+    public static final Color LIME_400 = new Color(0xD4E157);
+    public static final Color LIME_500 = new Color(0xCDDC39);
+    public static final Color LIME_600 = new Color(0xC0CA33);
+    public static final Color LIME_700 = new Color(0xAFB42B);
+    public static final Color LIME_800 = new Color(0x9E9D24);
+    public static final Color LIME_900 = new Color(0x827717);
+
+    // Yellow
+    public static final Color YELLOW_50 = new Color(0xFFFDE7);
+    public static final Color YELLOW_100 = new Color(0xFFFDE7);
+    public static final Color YELLOW_200 = new Color(0xFFF59D);
+    public static final Color YELLOW_300 = new Color(0xFFF176);
+    public static final Color YELLOW_400 = new Color(0xFFEE58);
+    public static final Color YELLOW_500 = new Color(0xFFEB3B);
+    public static final Color YELLOW_600 = new Color(0xFFE082);
+    public static final Color YELLOW_700 = new Color(0xFFD600);
+    public static final Color YELLOW_800 = new Color(0xF9A825);
+    public static final Color YELLOW_900 = new Color(0xF57F17);
+
+    // Amber
+    public static final Color AMBER_50 = new Color(0xFFF8E1);
+    public static final Color AMBER_100 = new Color(0xFFECB3);
+    public static final Color AMBER_200 = new Color(0xFFE082);
+    public static final Color AMBER_300 = new Color(0xFFD54F);
+    public static final Color AMBER_400 = new Color(0xFFCA28);
+    public static final Color AMBER_500 = new Color(0xFFC107);
+    public static final Color AMBER_600 = new Color(0xFFB300);
+    public static final Color AMBER_700 = new Color(0xFFA000);
+    public static final Color AMBER_800 = new Color(0xFF8F00);
+    public static final Color AMBER_900 = new Color(0xFF6F00);
+
+    // Orange
+    public static final Color ORANGE_50 = new Color(0xFFF3E0);
+    public static final Color ORANGE_100 = new Color(0xFFE0B2);
+    public static final Color ORANGE_200 = new Color(0xFFCC80);
+    public static final Color ORANGE_300 = new Color(0xFFB74D);
+    public static final Color ORANGE_400 = new Color(0xFFA726);
+    public static final Color ORANGE_500 = new Color(0xFF9800);
+    public static final Color ORANGE_600 = new Color(0xFB8C00);
+    public static final Color ORANGE_700 = new Color(0xF57C00);
+    public static final Color ORANGE_800 = new Color(0xEF6C00);
+    public static final Color ORANGE_900 = new Color(0xE65100);
+
+    // Deep Orange
+    public static final Color DEEP_ORANGE_50 = new Color(0xFBE9E7);
+    public static final Color DEEP_ORANGE_100 = new Color(0xFFCCBC);
+    public static final Color DEEP_ORANGE_200 = new Color(0xFFAB91);
+    public static final Color DEEP_ORANGE_300 = new Color(0xFF8A65);
+    public static final Color DEEP_ORANGE_400 = new Color(0xFF7043);
+    public static final Color DEEP_ORANGE_500 = new Color(0xFF5722);
+    public static final Color DEEP_ORANGE_600 = new Color(0xF4511E);
+    public static final Color DEEP_ORANGE_700 = new Color(0xE64A19);
+    public static final Color DEEP_ORANGE_800 = new Color(0xD84315);
+    public static final Color DEEP_ORANGE_900 = new Color(0xBF360C);
+
+    // Brown
+    public static final Color BROWN_50 = new Color(0xEFEBE9);
+    public static final Color BROWN_100 = new Color(0xD7CCC8);
+    public static final Color BROWN_200 = new Color(0xBCAAA4);
+    public static final Color BROWN_300 = new Color(0xA1887F);
+    public static final Color BROWN_400 = new Color(0x8D6E63);
+    public static final Color BROWN_500 = new Color(0x795548);
+    public static final Color BROWN_600 = new Color(0x6D4C41);
+    public static final Color BROWN_700 = new Color(0x5D4037);
+    public static final Color BROWN_800 = new Color(0x4E342E);
+    public static final Color BROWN_900 = new Color(0x3E2723);
+
+    // Accent Colors
+    public static final Color RED_A700 = new Color(0xD50000);
+    public static final Color PINK_A700 = new Color(0xC51162);
+    public static final Color PURPLE_A700 = new Color(0xAA00FF);
+    public static final Color DEEP_PURPLE_A700 = new Color(0x651FFF);
+    public static final Color INDIGO_A700 = new Color(0x304FFE);
+    public static final Color BLUE_A700 = new Color(0x2962FF);
+    public static final Color LIGHT_BLUE_A700 = new Color(0x0091EA);
+    public static final Color CYAN_A700 = new Color(0x00B8D4);
+    public static final Color TEAL_A700 = new Color(0x00BFA5);
+    public static final Color GREEN_A700 = new Color(0x00C853);
+    public static final Color LIGHT_GREEN_A700 = new Color(0x64DD17);
+    public static final Color LIME_A700 = new Color(0xAEEA00);
+    public static final Color YELLOW_A700 = new Color(0xFFD600);
+    public static final Color AMBER_A700 = new Color(0xFFC400);
+    public static final Color ORANGE_A700 = new Color(0xFF9100);
+    public static final Color DEEP_ORANGE_A700 = new Color(0xFF3D00);
+
+    // Grayscales
+    public static final Color WHITE = new Color(0xFFFFFF);
+    public static final Color GREY_50 = new Color(0xFAFAFA);
+    public static final Color GREY_100 = new Color(0xF5F5F5);
+    public static final Color GREY_200 = new Color(0xEEEEEE);
+    public static final Color GREY_300 = new Color(0xE0E0E0);
+    public static final Color GREY_400 = new Color(0xBDBDBD);
+    public static final Color GREY_500 = new Color(0x9E9E9E);
+    public static final Color GREY_600 = new Color(0x757575);
+    public static final Color GREY_700 = new Color(0x616161);
+    public static final Color GREY_800 = new Color(0x424242);
+    public static final Color GREY_900 = new Color(0x212121);
+    public static final Color BLACK = new Color(0x000000);
+
+    // Blue Grey
+    public static final Color BLUE_GREY_50 = new Color(0xECEFF1);
+    public static final Color BLUE_GREY_100 = new Color(0xCFD8DC);
+    public static final Color BLUE_GREY_200 = new Color(0xB0BEC5);
+    public static final Color BLUE_GREY_300 = new Color(0x90A4AE);
+    public static final Color BLUE_GREY_400 = new Color(0x78909C);
+    public static final Color BLUE_GREY_500 = new Color(0x607D8B);
+    public static final Color BLUE_GREY_600 = new Color(0x546E7A);
+    public static final Color BLUE_GREY_700 = new Color(0x455A64);
+    public static final Color BLUE_GREY_800 = new Color(0x37474F);
+    public static final Color BLUE_GREY_900 = new Color(0x263238);
+    
+    // Private constructor to prevent instantiation
+    private MaterialColors() {
+    }
+}
+
+
+
+
