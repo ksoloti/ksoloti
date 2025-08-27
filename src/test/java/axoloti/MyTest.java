fffdf11e5043c7469bd004be74d39abe52821e0e
sebiik <sebiik@gmail.com>
2025-08-27 17:54:01 +0800

    Remove non-functional test cases, add 'prove that it works' minimal test case MyTest (This is for the 'Test runner for Java" extension in VSCode, as neither ant test nor the netbeans-remnant test are set up in any coherent way)

---
+++ src/test/java/axoloti/MyTest.java
@@ -0,0 +1,12 @@
+package test.java.axoloti;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MyTest {
+
+    @Test
+    public void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
