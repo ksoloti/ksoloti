0611c8cd04be175bceefacc9ac86590fd5e3bc59
sebiik <sebiik@gmail.com>
2025-09-10 21:49:39 +0800

    Randomizer: add list of all parameters and only randomize the selected ones

--- src/main/java/axoloti/patch/MutatorFrame.java
+++ src/main/java/axoloti/patch/MutatorFrame.java
@@ -1,30 +1,61 @@
+/**
+ * Copyright (C) 2013 - 2016 Johannes Taelman
+ * Edited 2023 - 2024 by Ksoloti
+ *
+ * This file is part of Axoloti.
+ *
+ * Axoloti is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Axoloti is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Axoloti. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package axoloti.patch;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
 
 import axoloti.Patch;
+import axoloti.object.AxoObjectInstanceAbstract;
+import axoloti.parameters.ParameterInstance;
 
 /**
  * A GUI frame for the Ksoloti Patch Randomizer.
- * Provides buttons to apply randomization to patch parameters.
+ * Provides buttons and a parameter list to apply randomization to patch parameters.
+ * @author Ksoloti
  */
 public class MutatorFrame extends JFrame {
 
     private static final Logger LOGGER = Logger.getLogger(MutatorFrame.class.getName());
 
     private Patch patch;
+    private JList<ParameterInstance> parameterList;
 
     /**
      * Constructs a new MutatorFrame.
@@ -34,69 +65,120 @@ public class MutatorFrame extends JFrame {
         super("Ksoloti Patch Mutator");
         this.patch = patch;
         
-        // Set up the frame properties
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setMinimumSize(new Dimension(300, 150));
+        setMinimumSize(new Dimension(350, 450));
         
-        // Create the main panel with GridBagLayout for flexible arrangement
         JPanel mainPanel = new JPanel(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(10, 10, 10, 10); // Add some padding
+        gbc.insets = new Insets(10, 10, 10, 10);
 
-        // Add a descriptive label
-        JLabel titleLabel = new JLabel("Randomize all non-frozen parameters:");
+        JLabel titleLabel = new JLabel("Randomize selected parameters:");
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.gridwidth = 3;
         gbc.anchor = GridBagConstraints.CENTER;
         mainPanel.add(titleLabel, gbc);
 
-        // Add 10% randomization button
+        JLabel listLabel = new JLabel("Parameters:");
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 3;
+        gbc.anchor = GridBagConstraints.WEST;
+        mainPanel.add(listLabel, gbc);
+        listLabel.setVisible(false);
+        
+        DefaultListModel<ParameterInstance> listModel = new DefaultListModel<>();
+        if (patch.objectInstances.size() > 0) {
+            for (AxoObjectInstanceAbstract obj : patch.objectInstances) {
+                for (ParameterInstance param : obj.getParameterInstances()) {
+                    listModel.addElement(param);
+                }
+            }
+        }
+        
+        parameterList = new JList<>(listModel);
+        parameterList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        
+        parameterList.setCellRenderer(new DefaultListCellRenderer() {
+            @Override
+            public Component getListCellRendererComponent(JList<?> list, Object value, int index,
+                                                          boolean isSelected, boolean cellHasFocus) {
+                Component c = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+                ParameterInstance param = (ParameterInstance) value;
+                setText(param.GetObjectInstance().getInstanceName() + ":" + param.getName());
+                if (param.isFrozen()) {
+                    c.setForeground(Color.GRAY);
+                } else {
+                    c.setForeground(Color.BLACK);
+                }
+                return c;
+            }
+        });
+        
+        JScrollPane scrollPane = new JScrollPane(parameterList);
+        scrollPane.setPreferredSize(new Dimension(300, 200));
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 3;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        mainPanel.add(scrollPane, gbc);
+
         JButton button10 = new JButton("10%");
         button10.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                LOGGER.log(Level.INFO, "Randomizing by 10%");
-                PatchRandomizer.randomizeAllParameters(patch, 0.10f);
+                randomizeSelected(0.10f);
             }
         });
         gbc.gridx = 0;
-        gbc.gridy = 1;
+        gbc.gridy = 3;
         gbc.gridwidth = 1;
+        gbc.weightx = 0.33;
+        gbc.weighty = 0;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         mainPanel.add(button10, gbc);
 
-        // Add 25% randomization button
         JButton button25 = new JButton("25%");
         button25.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                LOGGER.log(Level.INFO, "Randomizing by 25%");
-                PatchRandomizer.randomizeAllParameters(patch, 0.25f);
+                randomizeSelected(0.25f);
             }
         });
         gbc.gridx = 1;
-        gbc.gridy = 1;
+        gbc.gridy = 3;
         mainPanel.add(button25, gbc);
 
-        // Add 50% randomization button
         JButton button50 = new JButton("50%");
         button50.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                LOGGER.log(Level.INFO, "Randomizing by 50%");
-                PatchRandomizer.randomizeAllParameters(patch, 0.50f);
+                randomizeSelected(0.50f);
             }
         });
         gbc.gridx = 2;
-        gbc.gridy = 1;
+        gbc.gridy = 3;
         mainPanel.add(button50, gbc);
-
-        // Add the panel to the frame
+        
         add(mainPanel);
         
-        // Finalize and show the frame
         pack();
-        setLocationRelativeTo(null); // Center the frame on the screen
+        setLocationRelativeTo(null);
+    }
+    
+    /**
+     * Helper method to get selected parameters and call the randomizer.
+     * @param percent The randomization percentage.
+     */
+    private void randomizeSelected(float percent) {
+        List<ParameterInstance> selectedParameters = parameterList.getSelectedValuesList();
+        if (selectedParameters.isEmpty()) {
+            LOGGER.log(Level.WARNING, "No parameters selected. Please select one or more parameters to randomize.");
+        } else {
+            LOGGER.log(Level.INFO, "Randomizing " + selectedParameters.size() + " selected parameters by " + (percent * 100) + "%");
+            PatchRandomizer.randomizeParameters(selectedParameters, percent);
+        }
     }
 }
--- src/main/java/axoloti/patch/PatchRandomizer.java
+++ src/main/java/axoloti/patch/PatchRandomizer.java
@@ -1,6 +1,26 @@
+/**
+ * Copyright (C) 2013 - 2016 Johannes Taelman
+ * Edited 2023 - 2024 by Ksoloti
+ *
+ * This file is part of Axoloti.
+ *
+ * Axoloti is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Axoloti is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Axoloti. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package axoloti.patch;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,10 +32,19 @@ import axoloti.datatypes.ValueInt32;
 import axoloti.object.AxoObjectInstanceAbstract;
 import axoloti.parameters.ParameterInstance;
 
+/**
+ * 
+ * @author Ksoloti
+ */
 public class PatchRandomizer {
     private static final Logger LOGGER = Logger.getLogger(PatchRandomizer.class.getName());
     private static Random random = new Random(Instant.EPOCH.getEpochSecond());
 
+    /**
+     * Randomizes all non-frozen parameters in a patch.
+     * @param patch The patch to randomize.
+     * @param percent The percentage to apply randomization by.
+     */
     public static void randomizeAllParameters(Patch patch, float percent) { /* percent: 0.25f equals 25% */
         if (patch.objectInstances.size() > 0) {
             for (AxoObjectInstanceAbstract obj : patch.objectInstances) {
@@ -39,6 +68,35 @@ public class PatchRandomizer {
             }
             LOGGER.log(Level.INFO, "");
             patch.SetDirty(true);
+        }
+    }
+
+    /**
+     * Randomizes a list of selected parameters.
+     * @param selectedParameters The list of parameters to randomize.
+     * @param percent The percentage to apply randomization by.
+     */
+    public static void randomizeParameters(List<ParameterInstance> selectedParameters, float percent) {
+        if (selectedParameters != null && !selectedParameters.isEmpty()) {
+            for (ParameterInstance param : selectedParameters) {
+                if (!param.isFrozen()) {
+                    int randomValue = (int)(random.nextInt() * percent)/16;
+                    param.SetValueRaw(param.GetValueRaw() + randomValue);
+                    param.SetNeedsTransmit(true);
+    
+                    String paramVal = "";
+                    Value vl = param.getValue();
+                    if (vl instanceof ValueFrac32) {
+                        paramVal += String.format("%.2f", vl.getDouble());
+                    }
+                    else if (vl instanceof ValueInt32) {
+                        paramVal += vl.getInt();
+                    }
+                    LOGGER.log(Level.INFO, "Randomize " + param.GetObjectInstance().getInstanceName() + ":" + param.getName() + " " + paramVal);
+                } else {
+                    LOGGER.log(Level.INFO, "Skipping frozen parameter: " + param.GetObjectInstance().getInstanceName() + ":" + param.getName());
+                }
+            }
         }
     }
 }
