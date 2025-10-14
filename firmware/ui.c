c87bc58bd2f00ec2037b416da1cf60fd1859dbf4
sebiik <sebiik@gmail.com>
2025-10-14 16:05:20 +0800

    Reduce ThreadUI working area size

--- firmware/ui.c
+++ firmware/ui.c
@@ -178,7 +178,7 @@ void KVP_RegisterObject(KeyValuePair_s *kvp) {
 }
 #endif /* USE_KVP */
 
-static WORKING_AREA(waThreadUI, 2048);
+static WORKING_AREA(waThreadUI, 1500);
 
 __attribute__((noreturn)) static msg_t ThreadUI(void *arg) {
     (void)(arg);
