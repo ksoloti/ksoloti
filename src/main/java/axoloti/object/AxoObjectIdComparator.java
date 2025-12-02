ab2adea4029c69e5d6d30bba367a5eb0af545011
sebiik <sebiik@gmail.com>
2025-12-02 23:37:59 +0800

    Add new line

--- src/main/java/axoloti/object/AxoObjectIdComparator.java
+++ src/main/java/axoloti/object/AxoObjectIdComparator.java
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License along with
  * Ksoloti. If not, see <http://www.gnu.org/licenses/>.
  */
+
 package axoloti.object;
 
 import java.util.Comparator;
