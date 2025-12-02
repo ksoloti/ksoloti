17638dbe18bfc0565b15f57d18a31bc01119d072
sebiik <sebiik@gmail.com>
2025-12-02 17:21:04 +0800

    Add new line

--- src/main/java/axoloti/Axoloti.java
+++ src/main/java/axoloti/Axoloti.java
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU General Public License along with
  * Axoloti. If not, see <http://www.gnu.org/licenses/>.
  */
+
 package axoloti;
 
 import axoloti.dialogs.AxoSplashScreen;
