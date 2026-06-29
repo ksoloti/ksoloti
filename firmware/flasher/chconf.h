d61c0926dce977f8cc2bc63c403044d52735c863
sebiik <sebiik@gmail.com>
2026-06-29 14:57:04 +0800

    Revert "Enable CH_DBG_ENABLE_CHECKS, CH_DBG_ENABLE_ASSERTS"
    This reverts commit 204abaadcf490cece1f930e1137f1c040c83b1ff.

--- firmware/flasher/chconf.h
+++ firmware/flasher/chconf.h
@@ -560,7 +560,7 @@
  * @note    The default is @p FALSE.
  */
 #if !defined(CH_DBG_SYSTEM_STATE_CHECK) || defined(__DOXYGEN__)
-#define CH_DBG_SYSTEM_STATE_CHECK       TRUE
+#define CH_DBG_SYSTEM_STATE_CHECK       FALSE
 #endif
 
 /**
@@ -583,7 +583,7 @@
  * @note    The default is @p FALSE.
  */
 #if !defined(CH_DBG_ENABLE_ASSERTS) || defined(__DOXYGEN__)
-#define CH_DBG_ENABLE_ASSERTS           TRUE
+#define CH_DBG_ENABLE_ASSERTS           FALSE
 #endif
 
 /**
