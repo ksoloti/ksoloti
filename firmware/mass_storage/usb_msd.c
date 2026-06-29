99bec30f2f2a30fb5f04d6562cd1b3b46aee311f
sebiik <sebiik@gmail.com>
2026-06-30 02:10:51 +0800

    Increase working area size of mass storage thread, increase priority

--- firmware/mass_storage/usb_msd.c
+++ firmware/mass_storage/usb_msd.c
@@ -865,9 +865,9 @@ int msdStart(USBMassStorageDriver *msdp, const USBMassStorageConfig *config) {
     config->usbp->in_params[config->bulk_ep] = (void *)msdp;
     config->usbp->out_params[config->bulk_ep] = (void *)msdp;
 
-    static WORKING_AREA(mass_storage_thread_wa, 1024);
+    static WORKING_AREA(mass_storage_thread_wa, 2048);
     /* run the thread */
-    msdp->thread = chThdCreateStatic(mass_storage_thread_wa, sizeof(mass_storage_thread_wa), NORMALPRIO, (void*) mass_storage_thread, msdp);
+    msdp->thread = chThdCreateStatic(mass_storage_thread_wa, sizeof(mass_storage_thread_wa), HIGHPRIO-1, (void*) mass_storage_thread, msdp);
     
     return 0;
 }
